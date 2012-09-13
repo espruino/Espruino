4ae21928d32df4b14a3a10481d2d72c1d7968d9a
Gordon Williams (u36) <gw@pur3.co.uk>
2012-09-13 14:09:32 +0100

    fix problem JSONing functions

--- src/TinyJSC.c
+++ src/TinyJSC.c
@@ -1,6 +1,7 @@
 /* REQUIRES -std=c99 FOR COMPILATION!
  *
  * TODO:
+ *       When entering stuff interactively, newlines are not kept (try entering a function)
  *       On assert fail, should restart interpreter and try and recover
  *       Make save() retry writing to flash (and not even bother if it was correct)
  *       Detect if running out of FIFO space and skip writing characters
