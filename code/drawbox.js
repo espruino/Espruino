cd4939bd643173c566818ef433a83d077d3b2aea
Gordon Williams (u36) <gw@pur3.co.uk>
2012-09-13 17:07:59 +0100

    updated draw box

--- code/drawbox.js
+++ code/drawbox.js
@@ -11,36 +11,6 @@ function boxcoords(i) {
   }
 }
 
-//for (i=0;i<4;i+=0.1) print(JSON.stringify(boxcoords(i)));
-
-//clearInterval(0);
-
-var pos = 0.0;
-var coords = [];
-setInterval(function() {
- pos+=0.01;
- coords = boxcoords(pos);
- print("1");
- pulse("A1",1,1+coords[0]);
- pulse("A2",1,1+coords[1]);
- print("0");
-},100);
-
-
-// -------------------------------- The following snippet KILLS IT
-function boxcoords(i) {
-  while (i>4) i-=4;
-  if (i<1) {
-    return [i,0];
-  } else if (i<2) {
-    return [1,i-1];
-  } else if (i<3) {
-    return [3-i,1];
-  } else {
-    return [0,4-i];
-  }
-}
-
 clearInterval(0);
 var pos = 0.0;
 var coords = [];
@@ -51,7 +21,10 @@ setInterval(function() {
  pulse("A2",1,1+coords[1]);
 },20);
 
-setTimeout(function() { 
+setWatch(function() { 
+  if (digitalRead("A0")) { 
+    digitalWrite("C9",1); 
+    setTimeout(function() { 
       digitalWrite("C9", 0);
       setTimeout(function() { 
         digitalWrite("C9", 1);
@@ -60,14 +33,5 @@ setTimeout(function() {
         }, 301);
       }, 302);
     }, 303);
+}}, "A0", true);
 
-setTimeout(function() { 
-      digitalWrite("C9", 0);
-      setTimeout(function() { 
-        digitalWrite("C9", 1);
-        setTimeout(function() { 
-          digitalWrite("C9", 0);
-        }, 301);
-      }, 302);
-    }, 303);
-// -------------------------------- The following snippet KILLS IT
