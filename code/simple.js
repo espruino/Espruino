e0e2746ec2d6c8552ce1d9aa6e5700aeb3a31c81
Gordon Williams (u36) <gw@pur3.co.uk>
2012-10-04 14:01:30 +0100

    code

--- code/simple.js
+++ code/simple.js
@@ -1,1 +1,20 @@
 function foo(a,b) { return a+333*b;};foo(1,4);
+
+
+
+function flash(c) {
+  if (c<=0) return;
+  setTimeout(function() {
+    digitalWrite("C9",1);
+    setTimeout(function() {
+      digitalWrite("C9",0);
+      flash(c-1);
+    }, 250);
+  }, 250);
+}
+
+var count = 0;
+setWatch(function() {
+ if (!digitalRead("A0")) return;
+ flash(count++);
+}, "A0", true);
