2a0f6fef98fafbd528b98ccd7066e50098058fe4
Gordon Williams <gw@pur3.co.uk>
2013-10-10 11:11:34 +0100

    More arraybuffer iteration fixes

--- tests/test_arraybuffer_offsets.js
+++ tests/test_arraybuffer_offsets.js
@@ -6,9 +6,10 @@ var b = new Int8Array(buf, 3);
 var c = new Int8Array(buf, 1,1);
 a[4] = 5;
 b[2] = 6;
+c[0] = 42; 
 c[1] = 7; // Ignored as out of range
 
 r = ""+a;
 l = c.byteLength;
 
-result= r == "0,0,0,0,5,6" && l==1;
+result= r == "0,42,0,0,5,6" && l==1;
