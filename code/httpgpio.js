92653e04d1babbc03e9ba472e0563d5fc3a45422
Gordon Williams <gw@pur3.co.uk>
2013-11-18 18:36:22 +0000

    more useful http gpio demo

--- code/httpgpio.js
+++ code/httpgpio.js
@@ -3,11 +3,11 @@
 function onPageRequest(req, res) {
   res.writeHead(200, {'Content-Type': 'text/html'});
   res.write('<html><body>');
-  res.write('<p>Pin is '+(D25.read()?'on':'off')+'</p>');
+  res.write('<p>Pin is '+(BTN.read()?'on':'off')+'</p>');
   res.write('<a href="/on">on</a><br/><a href="/off">off</a>');
   res.end('</body></html>');
-  if (req.url=="/on") digitalWrite(D7, 1);
-  if (req.url=="/off") digitalWrite(D7, 0);
+  if (req.url=="/on") digitalWrite(LED1, 1);
+  if (req.url=="/off") digitalWrite(LED1, 0);
 }
 http.createServer(onPageRequest).listen(8080);
 
