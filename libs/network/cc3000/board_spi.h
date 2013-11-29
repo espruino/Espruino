0b4727497777d565d37f266d330c512b321014ee
Gordon Williams (u36) <gw@pur3.co.uk>
2013-11-29 10:46:00 +0000

    doc

--- libs/network/cc3000/board_spi.h
+++ libs/network/cc3000/board_spi.h
@@ -34,7 +34,7 @@ extern int errno;
 #define WLAN_CLK_PIN      (Pin)(JSH_PORTB_OFFSET + 3)
 #define WLAN_MISO_PIN     (Pin)(JSH_PORTB_OFFSET + 4)
 #define WLAN_MOSI_PIN     (Pin)(JSH_PORTB_OFFSET + 5)
-#define WLAN_EN_PIN       (Pin)(JSH_PORTB_OFFSET + 7)
+#define WLAN_EN_PIN       (Pin)(JSH_PORTB_OFFSET + 7) // high to enable
 #define WLAN_IRQ_PIN      (Pin)(JSH_PORTB_OFFSET + 8) // active low
 #define WLAN_CS_PIN       (Pin)(JSH_PORTB_OFFSET + 6)
 
