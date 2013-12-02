b415720fb5f56475368d0c125590c55991d21441
Gordon Williams <gw@pur3.co.uk>
2013-12-02 16:40:59 +0000

    reference bug

--- tests/test055.js
+++ tests/test055.js
@@ -1,4 +1,6 @@
 // test  that setTimeout acts like jsConsole/Chrome's
+// HOWEVER this is not always the case. See what is reported on this bug: https://github.com/espruino/Espruino/issues/134
+// It does make a lot of sense for Espruino to do this though (see the comments in the bug)
 
 var result=0;
 function a() {result=0;}
