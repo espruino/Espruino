0ba7c6083424136cad6efaff2b1d469751c10a03
Gordon Williams <gw@pur3.co.uk>
2014-02-13 13:58:24 +0000

    Stop FatFS using insane amounts of RAM

--- libs/fat_sd/ffconf.h
+++ libs/fat_sd/ffconf.h
@@ -152,7 +152,7 @@
 
 
 #define	_MIN_SS		512
-#define	_MAX_SS		2048
+#define	_MAX_SS		512
 /* These options configure the sector size to be supported. (512, 1024, 2048 or 4096)
 /  Always set both 512 for most systems, all memory card and hard disk. But a larger
 /  value may be required for on-board flash memory and some type of optical media.
---
+++ scripts/find_big_ram.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Extracts and orders symbol names so we can see how big they are
+#
+
+if [ $# -eq 0 ]
+then
+  echo "USAGE:"
+  echo "scripts/find_big_ram.sh espruino_XXXX.lst"
+  exit 1
+fi
+
+grep "^20...... [^<]" $1 |  sort --key=4 
