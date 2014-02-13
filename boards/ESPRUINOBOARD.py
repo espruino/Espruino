35aad700bcefe2a91223fd213b07cae31c15be9e
Gordon Williams <gw@pur3.co.uk>
2014-02-13 11:46:01 +0000

    Increase available stack on Espruino Board

--- boards/ESPRUINOBOARD.py
+++ boards/ESPRUINOBOARD.py
@@ -17,7 +17,7 @@ import pinutils;
 info = {
  'name' : "Espruino Board rev 1.3",
  'link' : [ "http://www.espruino.com/kick" ],
- 'variables' : 2000,
+ 'variables' : 1800,
  'bootloader' : 1,
  'serial_bootloader' : True,
  'binary_name' : 'espruino_%v_espruino_1r3.bin',
