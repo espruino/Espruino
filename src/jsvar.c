288a9a3ec1709a89f3c1c7565620c7f51d5027af
Gordon Williams <gw@pur3.co.uk>
2014-04-08 09:19:27 +0100

    Fix conversion of floats to booleans (fix #307)

--- src/jsvar.c
+++ src/jsvar.c
@@ -1073,6 +1073,10 @@ bool jsvGetBool(const JsVar *v) {
     return jsvGetStringLength((JsVar*)v)!=0;
   if (jsvIsFunction(v) || jsvIsArray(v) || jsvIsObject(v))
     return true;
+  if (jsvIsFloat(v)) {
+    JsVarFloat f = jsvGetFloat(v);
+    return !isnan(f) && f!=0.0;
+  }
   return jsvGetInteger(v)!=0;
 }
 
