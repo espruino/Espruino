29f7eed9f0e21e7edeebe566ff4696ea1d3f7af9
Gordon Williams <gw@pur3.co.uk>
2014-04-30 11:20:44 +0100

    Fix String.split issue with last element (fix #352)

--- tests/test_string_split.js
+++ tests/test_string_split.js
@@ -2,4 +2,7 @@
 var b = "1,4,7";
 var a = b.split(",");
 
-result = a.length==3 && a[0]==1 && a[1]==4 && a[2]==7;
+var c = "Hello There".split("There");
+
+
+result = a.length==3 && a[0]==1 && a[1]==4 && a[2]==7 && c.length==2 && c[0]=="Hello " && c[1]=="";
