1929eb44950bad2536b0f5d250d6b4a703b2841e
Gordon Williams <gw@pur3.co.uk>
2014-05-21 10:01:37 +0100

    Improve Math.pow accuracy for small integer powers (fix #374)

--- libs/math/jswrap_math.c
+++ libs/math/jswrap_math.c
@@ -146,6 +146,24 @@ double jswrap_math_mod(double x, double y) {
 
 double jswrap_math_pow(double x, double y) {
   double p;
+  /* quick hack for raising to a small integer power.
+   * exp/log aren't accurate and are relatively slow, so
+   * it's probably better to bash through small integer
+   * powers in a stupid way. */
+  int yi = (int)y;
+  if (yi>=0 && yi<10 && yi==y) {
+    if (yi==0) return 1.0;
+    p = x;
+    while (yi>1) {
+      p *= x;
+      yi--;
+    }
+    return p;
+  }
+
+  /* do proper floating point pow. Not as accurate as a
+   * proper pow implementation but this saves a *lot*
+   * of flash */
   if (x < 0 && jswrap_math_mod(y, 1) == 0) {
     if (jswrap_math_mod(y, 2) == 0) {
       p = exp(log(-x) * y);
