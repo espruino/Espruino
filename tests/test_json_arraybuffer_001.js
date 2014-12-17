c45f3e988dccba83a876095506fe17f15f292069
Gordon Williams <gw@pur3.co.uk>
2014-12-17 11:46:12 +0000

    fix arraybuffer test

--- tests/test_json_arraybuffer_001.js
+++ tests/test_json_arraybuffer_001.js
@@ -1,3 +1,5 @@
 // currently has wrong number of elements (iterator issue?)
 
-result = JSON.stringify(new Uint8Array(1))=="new Uint8Array([0])";
+var a = new Uint8Array(2);
+a[1] = 1;
+result = JSON.stringify(new Uint8Array(1))=="new Uint8Array(1)" && JSON.stringify(a)=="new Uint8Array([0,1])";
