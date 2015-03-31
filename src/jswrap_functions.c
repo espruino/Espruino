bf40f34e8c6332fcc92ff180ada8168c8e2a3edb
Gordon Williams <gw@pur3.co.uk>
2015-03-31 11:44:05 +0100

    Propagate `this` into eval (fix #513)

--- src/jswrap_functions.c
+++ src/jswrap_functions.c
@@ -105,7 +105,7 @@ Evaluate a string containing JavaScript code
 JsVar *jswrap_eval(JsVar *v) {
   if (!v) return 0;
   JsVar *s = jsvAsString(v, false); // get as a string
-  JsVar *result = jspEvaluateVar(s, 0, false);
+  JsVar *result = jspEvaluateVar(s, execInfo.thisVar, false);
   jsvUnLock(s);
   return result;
 }
