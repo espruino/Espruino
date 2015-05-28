5a35049c4e08f48acd2f69cf929db644766b4c3e
Gordon Williams <gw@pur3.co.uk>
2015-05-28 15:09:57 +0100

    whoops - more error hackery

--- targetlibs/stm32f4/lib/stm32f4xx_conf.h
+++ targetlibs/stm32f4/lib/stm32f4xx_conf.h
@@ -89,7 +89,9 @@
 /* Exported functions ------------------------------------------------------- */
   void assert_failed(uint8_t* file, uint32_t line);
 #else
-  #define assert_param(expr) ((void)0)
+  #ifndef assert_param
+    #define assert_param(expr) ((void)0)
+  #endif
 #endif /* USE_FULL_ASSERT */
 
 #endif /* __STM32F4xx_CONF_H */
