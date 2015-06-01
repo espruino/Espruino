1362d005d3d603f514ac0f0aca68a800f6ecb1c7
Gordon Williams <gw@pur3.co.uk>
2015-06-01 10:55:37 +0100

    Tidy up USB configuration

--- targetlibs/stm32usb/usbd_conf.h
+++ targetlibs/stm32usb/usbd_conf.h
@@ -69,10 +69,6 @@
 #define USBD_DEBUG_LEVEL     0
 /*---------- -----------*/
 #define USBD_LPM_ENABLED     0
-/*---------- -----------*/
-#define USBD_SELF_POWERED     1
-/*---------- -----------*/
-#define USBD_CDC_INTERVAL     1000
 
 /****************************************/
 /* #define for FS and HS identification */
