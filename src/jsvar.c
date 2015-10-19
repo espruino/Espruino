beb4b6eb44687e6ad633e329b0a05e0637188f7d
Gordon Williams <gw@pur3.co.uk>
2015-10-19 16:25:54 +0100

    docs

--- src/jsvar.c
+++ src/jsvar.c
@@ -1113,7 +1113,7 @@ size_t jsvGetCharsOnLine(JsVar *v, size_t line) {
   return chars;
 }
 
-//  IN A STRING, get the line and column of the given character. Both values must be non-null
+//  IN A STRING, get the 1-based line and column of the given character. Both values must be non-null
 void jsvGetLineAndCol(JsVar *v, size_t charIdx, size_t *line, size_t *col) {
   size_t x = 1;
   size_t y = 1;
