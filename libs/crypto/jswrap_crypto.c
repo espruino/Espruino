1cdec0aec139929f1fbf761bce7b183e9463b69f
Gordon Williams <gw@pur3.co.uk>
2015-11-09 09:18:59 +0000

    doc tweaks

--- libs/crypto/jswrap_crypto.c
+++ libs/crypto/jswrap_crypto.c
@@ -28,8 +28,9 @@
 }
 Cryptographic functions
 
-**Note:** This library is currently only included in builds for the original Espruino boards.
-For other boards you will have to make build your own firmware.
+**Note:** This library is currently only included in builds for the Espruino Pico.
+For other boards you will have to make build your own firmware, and you may need
+to remove other features in order to make room.
 */
 
 
