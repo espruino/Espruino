f5f014f66bfa8c01574a87a51b3c6e3ae765d8b6
Gordon Williams <gw@pur3.co.uk>
2015-11-26 15:27:51 +0000

    Ensure Modules.addCached doesn't reset parse state (fix #723)

--- src/jsparse.c
+++ src/jsparse.c
@@ -2524,10 +2524,12 @@ JsVar *jspEvaluateModule(JsVar *moduleContents) {
   JsVar *exportsName = jsvAddNamedChild(scope, scopeExports, "exports");
   jsvUnLock2(scopeExports, jsvAddNamedChild(scope, scope, "module"));
 
+  JsExecFlags oldExecute = execInfo.execute;
   JsVar *oldThisVar = execInfo.thisVar;
   execInfo.thisVar = scopeExports; // set 'this' variable to exports
   jsvUnLock(jspEvaluateVar(moduleContents, scope, 0));
   execInfo.thisVar = oldThisVar;
+  execInfo.execute = oldExecute; // make sure we fully restore state after parsing a module
 
   jsvUnLock(scope);
   return jsvSkipNameAndUnLock(exportsName);
