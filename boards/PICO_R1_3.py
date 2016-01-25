bbe76f689a8cab714e6eabf528c05c8ad997539a
Gordon Williams (u36) <gw@pur3.co.uk>
2016-01-25 09:59:04 +0000

    update to show it's for 1.4 too

--- boards/PICO_R1_3.py
+++ boards/PICO_R1_3.py
@@ -15,7 +15,7 @@
 
 import pinutils;
 info = {
- 'name' : "Espruino Pico rev 1.3",
+ 'name' : "Espruino Pico rev 1.3/1.4",
  'link' : [ "http://www.espruino.com/Pico" ],
  'espruino_page_link' : 'Pico',
  'default_console' : "EV_SERIAL1",
