f3bc4d9e64ecbc792d3bdc83cefcbf3abd271923
mjdietzx <mjdietzx@gmail.com>
2016-02-12 00:26:46 -0800

    More RAM avaialble in BOARD.py. Still need to calc precisley.

--- boards/NRF52832DK.py
+++ boards/NRF52832DK.py
@@ -24,7 +24,7 @@ info = {
  'default_console_rx' : "D8",
  'default_console_baudrate' : "9600",
  # Number of variables can be WAY higher on this board
- 'variables' : 1020, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ 'variables' : 2040, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'binary_name' : 'espruino_%v_nrf52832.bin',
  'build' : {
   'defines' : [
