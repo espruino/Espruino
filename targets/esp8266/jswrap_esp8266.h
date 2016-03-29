12a415246c2c659f4be0a620f0231b64496bc10f
Marcel Urbanek <Marcel Urbanek>
2016-03-29 20:15:18 +0200

    Added deepSleep and modemSleep

--- targets/esp8266/jswrap_esp8266.h
+++ targets/esp8266/jswrap_esp8266.h
@@ -41,4 +41,7 @@ void   jswrap_ESP8266_neopixelWrite(Pin pin, JsVar *jsArrayOfData);
 
 uint32_t crc32(uint8_t *buf, uint32_t len);
 
+void   jswrap_ESP8266_deepSleep(JsVar *jsMicros);
+void   jswrap_ESP8266_modemSleep();
+
 #endif /* TARGETS_ESP8266_JSWRAP_ESP8266_H_ */
