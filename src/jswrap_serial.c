8ed37fc8f97493d6eb703698d7a2de044d8a44a1
Mikael Ganehag Brorsson <mikael.brorsson@gmail.com>
2016-07-20 17:08:16 +0200

    Allow path option

--- src/jswrap_serial.c
+++ src/jswrap_serial.c
@@ -183,7 +183,7 @@ Unless `force` is set to true, changes in the connection state of the board
   "generate" : "jswrap_serial_setup",
   "params" : [
     ["baudrate","JsVar","The baud rate - the default is 9600"],
-    ["options","JsVar",["An optional structure containing extra information on initialising the serial port.","```{rx:pin,tx:pin,bytesize:8,parity:null/'none'/'o'/'odd'/'e'/'even',stopbits:1,flow:null/undefined/'none'/'xon'}```","You can find out which pins to use by looking at [your board's reference page](#boards) and searching for pins with the `UART`/`USART` markers.","Note that even after changing the RX and TX pins, if you have called setup before then the previous RX and TX pins will still be connected to the Serial port as well - until you set them to something else using digitalWrite"]]
+    ["options","JsVar",["An optional structure containing extra information on initialising the serial port.","```{rx:pin,tx:pin,bytesize:8,parity:null/'none'/'o'/'odd'/'e'/'even',stopbits:1,flow:null/undefined/'none'/'xon',path:null/undefined/string}```","You can find out which pins to use by looking at [your board's reference page](#boards) and searching for pins with the `UART`/`USART` markers.","Note that even after changing the RX and TX pins, if you have called setup before then the previous RX and TX pins will still be connected to the Serial port as well - until you set them to something else using digitalWrite"]]
   ]
 }
 Setup this Serial port with the given baud rate and options.
@@ -204,12 +204,14 @@ void jswrap_serial_setup(JsVar *parent, JsVar *baud, JsVar *options) {
 
   JsVar *parity = 0;
   JsVar *flow = 0;
+  JsVar *path;
   jsvConfigObject configs[] = {
       {"rx", JSV_PIN, &inf.pinRX},
       {"tx", JSV_PIN, &inf.pinTX},
       {"ck", JSV_PIN, &inf.pinCK},
       {"bytesize", JSV_INTEGER, &inf.bytesize},
       {"stopbits", JSV_INTEGER, &inf.stopbits},
+      {"path", JSV_STRING_0, &path},
       {"parity", JSV_OBJECT /* a variable */, &parity},
       {"flow", JSV_OBJECT /* a variable */, &flow},
   };
@@ -254,7 +256,7 @@ void jswrap_serial_setup(JsVar *parent, JsVar *baud, JsVar *options) {
 
 #ifdef LINUX
     if (ok && jsvIsObject(options))
-      jsvObjectSetChildAndUnLock(parent, "path", jsvObjectGetChild(options, "path", 0));
+      jsvObjectSetChildAndUnLock(parent, "path", path);
 #endif
   }
   jsvUnLock(parity);
