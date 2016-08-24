9daec160e8b4e883bb23cd35039247f5f534f7de
Gordon Williams <gw@pur3.co.uk>
2016-08-24 15:20:23 +0100

    grr. fix bootloader when working with >64k images

--- targetlibs/nrf5x/nrf52_config/pstorage_platform.h
+++ targetlibs/nrf5x/nrf52_config/pstorage_platform.h
@@ -48,10 +48,10 @@ static __INLINE uint32_t pstorage_flash_page_end()
 #endif
 #define PSTORAGE_MIN_BLOCK_SIZE     0x0010                                                      /**< Minimum size of block that can be registered with the module. Should be configured based on system requirements, recommendation is not have this value to be at least size of word. */
 
-#define PSTORAGE_DATA_START_ADDR    ((PSTORAGE_FLASH_PAGE_END - PSTORAGE_NUM_OF_PAGES - 1) \
+#define PSTORAGE_DATA_START_ADDR    ((PSTORAGE_FLASH_PAGE_END - PSTORAGE_NUM_OF_PAGES) \
                                     * PSTORAGE_FLASH_PAGE_SIZE)                                 /**< Start address for persistent data, configurable according to system requirements. */
-#define PSTORAGE_DATA_END_ADDR      ((PSTORAGE_FLASH_PAGE_END - 1) * PSTORAGE_FLASH_PAGE_SIZE)  /**< End address for persistent data, configurable according to system requirements. */
-#define PSTORAGE_SWAP_ADDR          PSTORAGE_DATA_END_ADDR                                      /**< Top-most page is used as swap area for clear and update. */
+#define PSTORAGE_DATA_END_ADDR      (PSTORAGE_FLASH_PAGE_END * PSTORAGE_FLASH_PAGE_SIZE)        /**< End address for persistent data, configurable according to system requirements. */
+#define PSTORAGE_SWAP_ADDR          PSTORAGE_DATA_END_ADDR
 
 #define PSTORAGE_MAX_BLOCK_SIZE     PSTORAGE_FLASH_PAGE_SIZE                                    /**< Maximum size of block that can be registered with the module. Should be configured based on system requirements. And should be greater than or equal to the minimum size. */
 #define PSTORAGE_CMD_QUEUE_SIZE     10                                                          /**< Maximum number of flash access commands that can be maintained by the module for all applications. Configurable. */
@@ -74,7 +74,7 @@ typedef struct
     pstorage_block_t    block_id;       /**< Block ID.*/
 } pstorage_handle_t;
 
-typedef uint16_t pstorage_size_t;      /** Size of length and offset fields. */
+typedef uint32_t pstorage_size_t;      /** Size of length and offset fields. */
 
 /**@brief Handles Flash Access Result Events. To be called in the system event dispatcher of the application. */
 void pstorage_sys_event_handler (uint32_t sys_evt);
