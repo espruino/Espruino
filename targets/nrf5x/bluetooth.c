567e26dc98de042e3a8c2d8d094d5f6454f4aa64
Gordon Williams <gw@pur3.co.uk>
2016-10-13 16:11:09 +0100

    BLE tweaks to allow pairing

--- targets/nrf5x/bluetooth.c
+++ targets/nrf5x/bluetooth.c
@@ -62,6 +62,8 @@
 #define NEXT_CONN_PARAMS_UPDATE_DELAY   APP_TIMER_TICKS(30000, APP_TIMER_PRESCALER) /**< Time between each call to sd_ble_gap_conn_param_update after the first call (30 seconds). */
 #define MAX_CONN_PARAMS_UPDATE_COUNT    3                                           /**< Number of attempts before giving up the connection parameter negotiation. */
 
+#define APP_FEATURE_NOT_SUPPORTED       BLE_GATT_STATUS_ATTERR_APP_BEGIN + 2        /**< Reply when unsupported features are requested. */
+
 // -----------------------------------------------------------------------------------
 // -----------------------------------------------------------------------------------
 
@@ -247,11 +249,67 @@ static void on_ble_evt(ble_evt_t * p_ble_evt)
           jsble_update_services();
         break;
 
+      case BLE_GAP_EVT_SEC_PARAMS_REQUEST:
+        // Pairing not supported
+        err_code = sd_ble_gap_sec_params_reply(m_conn_handle, BLE_GAP_SEC_STATUS_SUCCESS, NULL, NULL);
+        // or BLE_GAP_SEC_STATUS_PAIRING_NOT_SUPP to disable pairing
+        APP_ERROR_CHECK(err_code);
+        break; // BLE_GAP_EVT_SEC_PARAMS_REQUEST
+
       case BLE_GATTS_EVT_SYS_ATTR_MISSING:
         // No system attributes have been stored.
         err_code = sd_ble_gatts_sys_attr_set(m_conn_handle, NULL, 0, 0);
         APP_ERROR_CHECK(err_code);
         break;
+
+      case BLE_GATTC_EVT_TIMEOUT:
+          // Disconnect on GATT Client timeout event.
+          err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gattc_evt.conn_handle,
+                                           BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+          APP_ERROR_CHECK(err_code);
+          break; // BLE_GATTC_EVT_TIMEOUT
+
+      case BLE_GATTS_EVT_TIMEOUT:
+          // Disconnect on GATT Server timeout event.
+          err_code = sd_ble_gap_disconnect(p_ble_evt->evt.gatts_evt.conn_handle,
+                                           BLE_HCI_REMOTE_USER_TERMINATED_CONNECTION);
+          APP_ERROR_CHECK(err_code);
+          break; // BLE_GATTS_EVT_TIMEOUT
+
+      case BLE_EVT_USER_MEM_REQUEST:
+          err_code = sd_ble_user_mem_reply(p_ble_evt->evt.gattc_evt.conn_handle, NULL);
+          APP_ERROR_CHECK(err_code);
+          break; // BLE_EVT_USER_MEM_REQUEST
+
+      case BLE_GATTS_EVT_RW_AUTHORIZE_REQUEST:
+      {
+          ble_gatts_evt_rw_authorize_request_t  req;
+          ble_gatts_rw_authorize_reply_params_t auth_reply;
+
+          req = p_ble_evt->evt.gatts_evt.params.authorize_request;
+
+          if (req.type != BLE_GATTS_AUTHORIZE_TYPE_INVALID)
+          {
+              if ((req.request.write.op == BLE_GATTS_OP_PREP_WRITE_REQ)     ||
+                  (req.request.write.op == BLE_GATTS_OP_EXEC_WRITE_REQ_NOW) ||
+                  (req.request.write.op == BLE_GATTS_OP_EXEC_WRITE_REQ_CANCEL))
+              {
+                  if (req.type == BLE_GATTS_AUTHORIZE_TYPE_WRITE)
+                  {
+                      auth_reply.type = BLE_GATTS_AUTHORIZE_TYPE_WRITE;
+                  }
+                  else
+                  {
+                      auth_reply.type = BLE_GATTS_AUTHORIZE_TYPE_READ;
+                  }
+                  auth_reply.params.write.gatt_status = APP_FEATURE_NOT_SUPPORTED;
+                  err_code = sd_ble_gatts_rw_authorize_reply(p_ble_evt->evt.gatts_evt.conn_handle,
+                                                             &auth_reply);
+                  APP_ERROR_CHECK(err_code);
+              }
+          }
+      } break; // BLE_GATTS_EVT_RW_AUTHORIZE_REQUEST
+
 
       case BLE_EVT_TX_COMPLETE:
         // UART Transmit finished - we can try and send more data
