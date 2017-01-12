041507bf546bf45cd5d509792bd78bfdc7039569
Gordon Williams <gw@pur3.co.uk>
2017-01-12 10:54:02 +0000

    bump version

--- src/jsutils.h
+++ src/jsutils.h
@@ -26,9 +26,9 @@
 #include <math.h>
 
 #ifndef BUILDNUMBER
-#define JS_VERSION "1v90"
+#define JS_VERSION "1v91"
 #else
-#define JS_VERSION "1v90." BUILDNUMBER
+#define JS_VERSION "1v91." BUILDNUMBER
 #endif
 /*
   In code:
