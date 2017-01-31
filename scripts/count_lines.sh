38d80eff1e9c53366f1a704c19d14e19ba63ba68
Gordon Williams <gw@pur3.co.uk>
2017-01-31 16:26:09 +0000

    line counting tweaks

--- scripts/count_lines.sh
+++ scripts/count_lines.sh
@@ -3,4 +3,12 @@
 cd `dirname $0`
 cd ..
 
+find . | grep -f scripts/count_lines.include | grep -v -f scripts/count_lines.exclude
+
+echo "Total lines"
 find . | grep -f scripts/count_lines.include | grep -v -f scripts/count_lines.exclude | xargs cat | wc -l
+
+#Can also count by lines written
+#find . | grep -f scripts/count_lines.include | grep -v -f scripts/count_lines.exclude | xargs -I{} git blame {} | sed -ne "s/[^(]*(\([A-Za-z0-9]*\).*/\1/p" > blames
+#sort blames | uniq -c | sort -n
+
