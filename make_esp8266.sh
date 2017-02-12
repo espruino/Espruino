6e9092b10fc89a8c3f80c91996a71b63bf86c3d6
Lancer Net <Lancer@appledeMacBook-Air.local>
2017-02-12 19:24:55 +0800

    ignore add

--- make_esp8266.sh
+++ make_esp8266.sh
@@ -2,8 +2,8 @@
 export RELEASE=1
 export ESP8266_BOARD=1
 export FLASH_4MB=1
-export ESP8266_SDK_ROOT=/home/lancer/Desktop/Espruino/esp_iot_sdk_v2.0.0.p1
-export PATH=$PATH:/home/lancer/Desktop/Espruino/xtensa-lx106-elf/bin
+export ESP8266_SDK_ROOT=/media/psf/Home/Desktop/Espruino/esp_iot_sdk_v2.0.0.p1
+#export PATH=$PATH:/home/lancer/Desktop/Espruino/xtensa-lx106-elf/bin
 #export COMPORT=/dev/ttyUSB0
 make clean
 make $*
