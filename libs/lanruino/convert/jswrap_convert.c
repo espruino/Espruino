27d2996b49acf0a10d4618df4131814f9a691b3f
Lancer Net <Lancer@appledeMacBook-Air.local>
2017-02-13 19:57:22 +0800

    complete convert library
    add functions:
    floatToBytes
    doubleToBytes
    bytesToFloat
    bytesToDouble

--- libs/lanruino/convert/jswrap_convert.c
+++ libs/lanruino/convert/jswrap_convert.c
@@ -1,3 +1,14 @@
+/*
+ * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+ *
+ * Copyright (C) 2017 lancer
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 #include "jswrap_convert.h"
 
 /*JSON{
@@ -24,9 +35,98 @@ bool jswrap_convert_isLittleEndian(){
     //jsiConsolePrintf("sizeof JsVarFloat:%d\r\n",sizeof(JsVarFloat));//8
     return (*p=0x78);
 }
-
-//JsVar *jswrap_convert_floatToBytes(JsVar *floatVar);
-//JsVar *jswrap_convert_doubleToBytes(JsVar *doubleVar);
-//JsVarFloat jswrap_convert_BytesToFloat(JsVar *bytes);
-//JsVarFloat jswrap_convert_BytesToDouble(JsVar *bytes); 
-//JsVarInt jswrap_convert_BytesToInt32(JsVar *bytes);
+/*JSON{
+    "type":"staticmethod",
+    "class":"convert",
+    "name":"floatToBytes",
+    "generate":"jswrap_convert_floatToBytes",
+    "params":[
+        ["floatVar","JsVar",""]
+    ],
+    "return":["JsVar",""]
+}*/
+JsVar *jswrap_convert_floatToBytes(JsVar *floatVar){
+    if(jsvIsFloat(floatVar)){
+        float floatVal=(float)jsvGetFloat(floatVar);
+        JsVar *bytesVar=jsvNewEmptyArray();
+        uint8_t *ptr=&floatVal;
+        for(int i=0;i<sizeof(float);i++){
+            jsvArrayPushAndUnLock(bytesVar,jsvNewFromInteger(*(ptr+i)));
+        }   
+        return bytesVar;
+    }else{
+        jsExceptionHere(JSET_ERROR, "arguments must type of Float");
+        return 0;
+    }
+}
+/*JSON{
+    "type":"staticmethod",
+    "class":"convert",
+    "name":"doubleToBytes",
+    "generate":"jswrap_convert_doubleToBytes",
+    "params":[
+        ["doubleVar","JsVar",""]
+    ],
+    "return":["JsVar",""]
+}
+*/
+JsVar *jswrap_convert_doubleToBytes(JsVar *doubleVar){
+    if(jsvIsFloat(doubleVar)){
+        double doubleVal=(double)jsvGetFloat(doubleVar);
+        JsVar *bytesVar=jsvNewEmptyArray();
+        uint8_t *ptr=&doubleVal;
+        for(int i=0;i<sizeof(double);i++){
+            jsvArrayPushAndUnLock(bytesVar,jsvNewFromInteger(*(ptr+i)));
+        }   
+        return bytesVar;
+    }else{
+        jsExceptionHere(JSET_ERROR, "arguments must type of Float");
+        return 0;
+    }
+}
+/*JSON{
+    "type":"staticmethod",
+    "class":"convert",
+    "name":"bytesToFloat",
+    "generate":"jswrap_convert_bytesToFloat",
+    "params":[
+        ["bytes","JsVar",""]
+    ],
+    "return":["JsVar",""]
+}*/
+JsVar *jswrap_convert_bytesToFloat(JsVar *bytes){
+    if(jsvIsArray(bytes)&&jsvGetArrayLength(bytes)==sizeof(float)){
+        uint8_t buf[sizeof(float)];
+        float *ptr=buf;
+        for(int i=0;i<sizeof(float);i++){
+            buf[i]=(uint8_t)jsvGetInteger(jsvGetArrayItem(bytes,i));
+        }
+        return jsvNewFromFloat(*ptr);
+    }else{
+        jsExceptionHere(JSET_ERROR,"arguments must type of Array and Array length is %d",sizeof(float));
+        return 0;
+    }
+}
+/*JSON{
+    "type":"staticmethod",
+    "class":"convert",
+    "name":"bytesToDouble",
+    "generate":"jswrap_convert_bytesToDouble",
+    "params":[
+        ["bytes","JsVar",""]
+    ],
+    "return":["JsVar",""]
+}*/
+JsVar *jswrap_convert_bytesToDouble(JsVar *bytes){
+    if(jsvIsArray(bytes)&&jsvGetArrayLength(bytes)==sizeof(double)){
+        uint8_t buf[sizeof(double)];
+        double *ptr=buf;
+        for(int i=0;i<sizeof(double);i++){
+            buf[i]=(uint8_t)jsvGetInteger(jsvGetArrayItem(bytes,i));
+        }
+        return jsvNewFromFloat(*ptr);
+    }else{
+        jsExceptionHere(JSET_ERROR,"arguments must type of Array and Array length is %d",sizeof(double));
+        return 0;
+    }
+}
--- libs/lanruino/convert/jswrap_convert.h
+++ libs/lanruino/convert/jswrap_convert.h
@@ -1,10 +1,24 @@
+/*
+ * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+ *
+ * Copyright (C) 2017 lancer
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+#ifndef LIBS_LANRUINO_CONVERT_H_
+#define LIBS_LANRUINO_CONVERT_H_
 #include "jsvar.h"
-#include "jsvariterator.h"
 #include "jsinteractive.h"
 
 bool jswrap_convert_isLittleEndian();
+
 JsVar *jswrap_convert_floatToBytes(JsVar *floatVar);
 JsVar *jswrap_convert_doubleToBytes(JsVar *doubleVar);
-JsVarFloat jswrap_convert_BytesToFloat(JsVar *bytes);
-JsVarFloat jswrap_convert_BytesToDouble(JsVar *bytes); 
-JsVarInt jswrap_convert_BytesToInt32(JsVar *bytes);
+JsVar *jswrap_convert_bytesToFloat(JsVar *bytes);
+JsVar *jswrap_convert_bytesToDouble(JsVar *bytes);
+
+#endif
