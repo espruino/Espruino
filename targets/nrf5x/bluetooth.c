f0ece34ea31e510957d97d8c0fe5b0457e71881b
Gordon Williams <gw@pur3.co.uk>
2017-02-28 11:55:38 +0000

    attempt to try and stop random disconnects

--- targets/nrf5x/bluetooth.c
+++ targets/nrf5x/bluetooth.c
@@ -929,7 +929,7 @@ static void conn_params_init() {
     cp_init.next_conn_params_update_delay  = NEXT_CONN_PARAMS_UPDATE_DELAY;
     cp_init.max_conn_params_update_count   = MAX_CONN_PARAMS_UPDATE_COUNT;
     cp_init.start_on_notify_cccd_handle    = BLE_GATT_HANDLE_INVALID;
-    cp_init.disconnect_on_fail             = true;
+    cp_init.disconnect_on_fail             = false;
     cp_init.evt_handler                    = on_conn_params_evt;
     cp_init.error_handler                  = conn_params_error_handler;
 
