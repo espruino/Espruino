d274e6af7f9553e34b7444fac1926019267df120
wilberforce <github@wilberforce.co.nz>
2017-03-04 10:28:35 +1300

    provision-framework scripts

--- scripts/provision-framework.sh
+++ scripts/provision-framework.sh
@@ -22,11 +22,16 @@
 if [ $# -eq 0 ]
 then
   echo "USAGE:"
-  echo "source scripts/provision.sh {ARCH}"
+  echo "source scripts/provision.sh {BOARD}"
   return 0
 fi
 
-ARCH=$1
+BOARD=$1
+
+# called from user - define the board type for Makefile
+	if [ -z $TRAVIS] ; then
+	    eval export $BOARD=1
+	fi	
 
 if [ $ARCH = "ESP32" ]; then
     echo ESP32
@@ -38,37 +43,37 @@ if [ $ARCH = "ESP32" ]; then
         echo installing esp-idf folder
         curl -Ls https://github.com/espruino/EspruinoBuildTools/raw/master/esp32/deploy/esp-idf.tgz | tar xfz -
     fi
-    if hash xtensa-esp32-elf-gcc 2>/dev/null; then
+    if ! type xtensa-esp32-elf-gcc > /dev/null; then
         echo installing xtensa-esp32-elf-gcc
         curl -Ls https://dl.espressif.com/dl/xtensa-esp32-elf-linux64-1.22.0-61-gab8375a-5.2.0.tar.gz | tar xfz -
-        export PATH=$PATH:`pwd`/xtensa-esp32-elf/bin/
+	else
+		which xtensa-esp32-elf-gcc
     fi
     export ESP_IDF_PATH=`pwd`/esp-idf
     export ESP_APP_TEMPLATE_PATH=`pwd`/app
+	export PATH=$PATH:`pwd`/xtensa-esp32-elf/bin/
     return 1
-elif [ $ARCH = "ESP8266" ]; then
+elif [ $ARCH = "ESP8266_BOARD" ]; then
     echo ESP8266
     if [ ! -d "esp_iot_sdk_v2.0.0.p1" ]; then
         echo esp_iot_sdk_v2.0.0.p1
         curl -Ls http://s3.voneicken.com/esp_iot_sdk_v2.0.0.p1.tgx | tar Jxf -
     fi
-    if hash xtensa-lx106-elf-gcc 2>/dev/null; then
+	if ! type xtensa-lx106-elf-gcc > /dev/null; then
         echo installing xtensa-lx106-elf-gcc
         curl -Ls http://s3.voneicken.com/xtensa-lx106-elf-20160330.tgx | tar Jxf -
+	else
+		which xtensa
     fi
     export ESP8266_SDK_ROOT=`pwd`/esp_iot_sdk_v2.0.0.p1
     export PATH=$PATH:`pwd`/xtensa-lx106-elf/bin/
     return 1
-elif [ $ARCH = "LINUX" ]; then
+elif [ $ARCH = "LINUX_BUILD" ]; then
     echo LINUX
     return 1
 else
     # defaulting to ARM
     echo ARM
-    #GCC-ARM_OK=$(dpkg-query -W --showformat='${Status}\n' gcc-arm-embedded|grep "install ok installed")
-    #echo Checking for gcc-arm-embedded: $PKG_OK
-    #if [ "" == "$CC-ARM_OK" ]; then
-    # not sure if these sudos will work out ?
     if hash arm-none-eabi-gcc 2>/dev/null; then
         echo installing gcc-arm-embedded
         sudo add-apt-repository -y ppa:team-gcc-arm-embedded/ppa
