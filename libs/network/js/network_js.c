9d9f0177af1c70c078180717b85b78d03de0e083
Gordon Williams <gw@pur3.co.uk>
2017-04-28 16:08:44 +0100

    Ensure NetworkJS reports receive errors back correctly

--- libs/network/js/network_js.c
+++ libs/network/js/network_js.c
@@ -120,6 +120,12 @@ int net_js_recv(JsNetwork *net, int sckt, void *buf, size_t len) {
     if (r>(int)len) { r=(int)len; assert(0); }
     jsvGetStringChars(res, 0, (char*)buf, (size_t)r);
     // FIXME: jsvGetStringChars adds a 0 - does that actually write past the end of the array, or clip the data we get?
+  } else if (jsvIsInt(res)) {
+    r = jsvGetInteger(res);
+    if (r>=0) {
+      jsExceptionHere(JSET_ERROR, "JSNetwork.recv returned >=0");
+      r=-1;
+    }
   }
   jsvUnLock(res);
   return r;
