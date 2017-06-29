9324e10f6562c007f0d45f5c8af9142c742a08d5
Gordon Williams <gw@pur3.co.uk>
2017-06-29 10:08:33 +0100

    Puck.js: Added Puck.magTemp() function to get the magnetometer;'s temperature as well

--- libs/puckjs/jswrap_puck.h
+++ libs/puckjs/jswrap_puck.h
@@ -16,6 +16,7 @@
 void jswrap_puck_magOn();
 void jswrap_puck_magOff();
 JsVar *jswrap_puck_mag();
+JsVarInt jswrap_puck_magTemp();
 void jswrap_puck_IR(JsVar *data, Pin cathode, Pin anode);
 int jswrap_puck_capSense(Pin tx, Pin rx);
 JsVarFloat jswrap_puck_light();
