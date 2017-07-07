0d5f4d9c125579eb811f1745b141aa6ac529a3b7
Gordon Williams <gw@pur3.co.uk>
2017-07-07 11:21:52 +0100

    argh. forgot to commit

---
+++ src/jsflags.h
@@ -0,0 +1,39 @@
+/*
+ * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+ *
+ * Copyright (C) 2017 Gordon Williams <gw@pur3.co.uk>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ----------------------------------------------------------------------------
+ * JS Interpreter specific flags
+ * ----------------------------------------------------------------------------
+ */
+#ifndef JSFLAGS_H_
+#define JSFLAGS_H_
+#include "jsvar.h"
+
+
+typedef enum {
+  JSF_NONE,
+  JSF_DEEP_SLEEP          = 1<<0, // Allow deep sleep modes (also set by setDeepSleep)
+  JSF_PRETOKENISE         = 1<<1, // When adding functions, pre-minify them and tokenise reserved words
+} PACKED_FLAGS JsFlags;
+
+#define JSFLAG_NAMES "deepSleep\0pretokenise\0"
+// NOTE: \0 also added by compiler - two \0's are required!
+
+extern volatile JsFlags jsFlags;
+
+/// Get the state of a flag
+bool jsfGetFlag(JsFlags flag);
+/// Set the state of a flag
+void jsfSetFlag(JsFlags flag, bool isOn);
+/// Get a list of all flags and their status
+JsVar *jsfGetFlags();
+/// Set any of the specified flags
+void jsfSetFlags(JsVar *flags);
+
+#endif //JSFLAGS_H_
