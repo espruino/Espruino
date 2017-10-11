59a1fa700925d3b7af7c4e9732d2feff28ce79e2
Gordon Williams <gw@pur3.co.uk>
2017-10-11 16:31:01 +0100

    Bump nRF52-based boards variable count from 2000 to 2500 (fix #1215)

--- boards/HEXBADGE.py
+++ boards/HEXBADGE.py
@@ -22,8 +22,7 @@ info = {
  'default_console_tx' : "D7",
  'default_console_rx' : "D8",
  'default_console_baudrate' : "9600",
- # Number of variables can be WAY higher on this board
- 'variables' : 2000, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ 'variables' : 2500, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'bootloader' : 1,
  'binary_name' : 'espruino_%v_hexbadge.bin',
  'build' : {
--- boards/PUCKJS.py
+++ boards/PUCKJS.py
@@ -22,8 +22,7 @@ info = {
  'default_console_tx' : "D28",
  'default_console_rx' : "D29",
  'default_console_baudrate' : "9600",
- # Number of variables can be WAY higher on this board
- 'variables' : 2000, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
+ 'variables' : 2500, # How many variables are allocated for Espruino to use. RAM will be overflowed if this number is too high and code won't compile.
  'bootloader' : 1,
  'binary_name' : 'espruino_%v_puckjs.hex',
  'build' : {
