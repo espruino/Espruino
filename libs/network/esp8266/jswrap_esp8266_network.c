87ec0578c06e5146aea8f7bd16c9ac879ad9da5a
MaBecker <mark.becker@sbit-gmbh.de>
2018-01-13 09:09:26 +0100

    add #else for WIFI and NET DBG

--- libs/network/esp8266/jswrap_esp8266_network.c
+++ libs/network/esp8266/jswrap_esp8266_network.c
@@ -33,10 +33,14 @@
   // Normal debug
   #if WIFI_DBG > 0
     #define DBG(format, ...) os_printf(format, ## __VA_ARGS__)
+  #else
+    #define DBG(format, ...) do { } while(0)
   #endif
   // Verbose debug
   #if WIFI_DBG > 1
     #define DBGV(format, ...) os_printf(format, ## __VA_ARGS__)
+  #else
+    #define DBGV(format, ...) do { } while(0)
   #endif
 #endif
 
