9711672b4a1b4ad9c64823f2c0255cf8cc18ba57
Standa Opichal <opichals@gmail.com>
2018-01-21 16:50:24 +0100

    Updated to Wiznet/ioLibrary_Driver 3.1.3

--- libs/network/wiznet/Ethernet/wizchip_conf.c
+++ libs/network/wiznet/Ethernet/wizchip_conf.c
@@ -2,9 +2,19 @@
 //!
 //! \file wizchip_conf.c
 //! \brief WIZCHIP Config Header File.
-//! \version 1.0.0
+//! \version 1.0.1
 //! \date 2013/10/21
 //! \par  Revision history
+//!       <2015/02/05> Notice
+//!        The version history is not updated after this point.
+//!        Download the latest version directly from GitHub. Please visit the our GitHub repository for ioLibrary.
+//!        >> https://github.com/Wiznet/ioLibrary_Driver
+//!       <2014/05/01> V1.0.1  Refer to M20140501
+//!        1. Explicit type casting in wizchip_bus_readdata() & wizchip_bus_writedata()
+//            Issued by Mathias ClauBen.
+//!           uint32_t type converts into ptrdiff_t first. And then recoverting it into uint8_t*
+//!           For remove the warning when pointer type size is not 32bit.
+//!           If ptrdiff_t doesn't support in your complier, You should must replace ptrdiff_t into your suitable pointer type.
 //!       <2013/10/21> 1st Release
 //! \author MidnightCow
 //! \copyright
@@ -38,60 +48,106 @@
 //! THE POSSIBILITY OF SUCH DAMAGE.
 //
 //*****************************************************************************/
+//A20140501 : for use the type - ptrdiff_t
+#include <stddef.h>
+//
+
 #include "wizchip_conf.h"
+
+/////////////
+//M20150401 : Remove ; in the default callback function such as wizchip_cris_enter(), wizchip_cs_select() and etc.
+/////////////
+
 /**
  * @brief Default function to enable interrupt.
  * @note This function help not to access wrong address. If you do not describe this function or register any functions,
  * null function is called.
  */
-void 	  wizchip_cris_enter(void)           {};
+//void 	  wizchip_cris_enter(void)           {};
+void 	  wizchip_cris_enter(void)           {}
+
 /**
  * @brief Default function to disable interrupt.
  * @note This function help not to access wrong address. If you do not describe this function or register any functions,
  * null function is called.
  */
-void 	  wizchip_cris_exit(void)          {};
+//void 	  wizchip_cris_exit(void)          {};
+void 	  wizchip_cris_exit(void)          {}
+
 /**
  * @brief Default function to select chip.
  * @note This function help not to access wrong address. If you do not describe this function or register any functions,
  * null function is called.
  */
-void 	wizchip_cs_select(void)            {};
+//void 	wizchip_cs_select(void)            {};
+void 	wizchip_cs_select(void)            {}
+
 /**
  * @brief Default function to deselect chip.
  * @note This function help not to access wrong address. If you do not describe this function or register any functions,
  * null function is called.
  */
-void 	wizchip_cs_deselect(void)          {};
+//void 	wizchip_cs_deselect(void)          {};
+void 	wizchip_cs_deselect(void)          {}
+
 /**
  * @brief Default function to read in direct or indirect interface.
  * @note This function help not to access wrong address. If you do not describe this function or register any functions,
  * null function is called.
  */
-uint8_t wizchip_bus_readbyte(uint32_t AddrSel) { return * ((volatile uint8_t *) AddrSel); };
+ //M20150601 : Rename the function for integrating with W5300
+//uint8_t wizchip_bus_readbyte(uint32_t AddrSel) { return * ((volatile uint8_t *)((ptrdiff_t) AddrSel)); }
+iodata_t wizchip_bus_readdata(uint32_t AddrSel) { return * ((volatile iodata_t *)((ptrdiff_t) AddrSel)); }
+
 /**
  * @brief Default function to write in direct or indirect interface.
  * @note This function help not to access wrong address. If you do not describe this function or register any functions,
  * null function is called.
  */
- 
-void 	wizchip_bus_writebyte(uint32_t AddrSel, uint8_t wb)  { *((volatile uint8_t*)AddrSel) = wb; };
+//M20150601 : Rename the function for integrating with W5300
+//void 	wizchip_bus_writebyte(uint32_t AddrSel, uint8_t wb)  { *((volatile uint8_t*)((ptrdiff_t)AddrSel)) = wb; }
+void 	wizchip_bus_writedata(uint32_t AddrSel, iodata_t wb)  { *((volatile iodata_t*)((ptrdiff_t)AddrSel)) = wb; }
+
 /**
  * @brief Default function to read in SPI interface.
  * @note This function help not to access wrong address. If you do not describe this function or register any functions,
  * null function is called.
  */
-uint8_t wizchip_spi_readbyte(void)        {return 0;};
+//uint8_t wizchip_spi_readbyte(void)        {return 0;};
+uint8_t wizchip_spi_readbyte(void)        {return 0;}
+
 /**
  * @brief Default function to write in SPI interface.
  * @note This function help not to access wrong address. If you do not describe this function or register any functions,
  * null function is called.
  */
-void 	wizchip_spi_writebyte(uint8_t wb) {};
+//void 	wizchip_spi_writebyte(uint8_t wb) {};
+void 	wizchip_spi_writebyte(uint8_t wb) {}
+
+/**
+ * @brief Default function to burst read in SPI interface.
+ * @note This function help not to access wrong address. If you do not describe this function or register any functions,
+ * null function is called.
+ */
+//void 	wizchip_spi_readburst(uint8_t* pBuf, uint16_t len) 	{}; 
+void 	wizchip_spi_readburst(uint8_t* pBuf, uint16_t len) 	{}
+
+/**
+ * @brief Default function to burst write in SPI interface.
+ * @note This function help not to access wrong address. If you do not describe this function or register any functions,
+ * null function is called.
+ */
+//void 	wizchip_spi_writeburst(uint8_t* pBuf, uint16_t len) {};
+void 	wizchip_spi_writeburst(uint8_t* pBuf, uint16_t len) {}
 
 /**
  * @\ref _WIZCHIP instance
  */
+//
+//M20150401 : For a compiler didnot support a member of structure
+//            Replace the assignment of struct members with the assingment of array
+//
+/*
 _WIZCHIP  WIZCHIP =
       {
       .id                  = _WIZCHIP_ID_,
@@ -105,6 +161,24 @@ _WIZCHIP  WIZCHIP =
 //    .IF.SPI._read_byte   = wizchip_spi_readbyte,
 //    .IF.SPI._write_byte  = wizchip_spi_writebyte
       };
+*/      
+_WIZCHIP  WIZCHIP =
+      {
+      _WIZCHIP_IO_MODE_,
+      _WIZCHIP_ID_ ,
+      wizchip_cris_enter,
+      wizchip_cris_exit,
+      wizchip_cs_select,
+      wizchip_cs_deselect,
+      //M20150601 : Rename the function 
+      //wizchip_bus_readbyte,
+      //wizchip_bus_writebyte
+      wizchip_bus_readdata,
+      wizchip_bus_writedata,
+//    wizchip_spi_readbyte,
+//    wizchip_spi_writebyte
+      };
+
 
 static uint8_t    _DNS_[4];      // DNS server ip address
 static dhcp_mode  _DHCP_;        // DHCP mode
@@ -137,10 +211,13 @@ void reg_wizchip_cs_cbfunc(void(*cs_sel)(void), void(*cs_desel)(void))
    }
 }
 
-void reg_wizchip_bus_cbfunc(uint8_t(*bus_rb)(uint32_t addr), void (*bus_wb)(uint32_t addr, uint8_t wb))
+//M20150515 : For integrating with W5300
+//void reg_wizchip_bus_cbfunc(uint8_t(*bus_rb)(uint32_t addr), void (*bus_wb)(uint32_t addr, uint8_t wb))
+void reg_wizchip_bus_cbfunc(iodata_t(*bus_rb)(uint32_t addr), void (*bus_wb)(uint32_t addr, iodata_t wb))
 {
    while(!(WIZCHIP.if_mode & _WIZCHIP_IO_MODE_BUS_));
-   
+   //M20150601 : Rename call back function for integrating with W5300
+   /*
    if(!bus_rb || !bus_wb)
    {
       WIZCHIP.IF.BUS._read_byte   = wizchip_bus_readbyte;
@@ -150,6 +227,17 @@ void reg_wizchip_bus_cbfunc(uint8_t(*bus_rb)(uint32_t addr), void (*bus_wb)(uint
    {
       WIZCHIP.IF.BUS._read_byte   = bus_rb;
       WIZCHIP.IF.BUS._write_byte  = bus_wb;
+   }
+   */
+   if(!bus_rb || !bus_wb)
+   {
+      WIZCHIP.IF.BUS._read_data   = wizchip_bus_readdata;
+      WIZCHIP.IF.BUS._write_data  = wizchip_bus_writedata;
+   }
+   else
+   {
+      WIZCHIP.IF.BUS._read_data   = bus_rb;
+      WIZCHIP.IF.BUS._write_data  = bus_wb;
    }
 }
 
@@ -169,9 +257,28 @@ void reg_wizchip_spi_cbfunc(uint8_t (*spi_rb)(void), void (*spi_wb)(uint8_t wb))
    }
 }
 
+// 20140626 Eric Added for SPI burst operations
+void reg_wizchip_spiburst_cbfunc(void (*spi_rb)(uint8_t* pBuf, uint16_t len), void (*spi_wb)(uint8_t* pBuf, uint16_t len))
+{
+   while(!(WIZCHIP.if_mode & _WIZCHIP_IO_MODE_SPI_));
+
+   if(!spi_rb || !spi_wb)
+   {
+      WIZCHIP.IF.SPI._read_burst   = wizchip_spi_readburst;
+      WIZCHIP.IF.SPI._write_burst  = wizchip_spi_writeburst;
+   }
+   else
+   {
+      WIZCHIP.IF.SPI._read_burst   = spi_rb;
+      WIZCHIP.IF.SPI._write_burst  = spi_wb;
+   }
+}
+
 int8_t ctlwizchip(ctlwizchip_type cwtype, void* arg)
 {
+#if	_WIZCHIP_ == 5200 || _WIZCHIP_ == 5500
    uint8_t tmp = 0;
+#endif
    uint8_t* ptmp[2] = {0,0};
    switch(cwtype)
    {
@@ -184,7 +291,7 @@ int8_t ctlwizchip(ctlwizchip_type cwtype, void* arg)
             ptmp[0] = (uint8_t*)arg;
             ptmp[1] = ptmp[0] + _WIZCHIP_SOCK_NUM_;
          }
-         return wizchip_init(ptmp[0], ptmp[1]);;
+         return wizchip_init(ptmp[0], ptmp[1]);
       case CW_CLR_INTERRUPT:
          wizchip_clrinterrupt(*((intr_kind*)arg));
          break;
@@ -197,7 +304,9 @@ int8_t ctlwizchip(ctlwizchip_type cwtype, void* arg)
       case CW_GET_INTRMASK:
          *((intr_kind*)arg) = wizchip_getinterruptmask();
          break;
-   #if _WIZCHIP_ > 5100
+   //M20150601 : This can be supported by W5200, W5500
+   //#if _WIZCHIP_ > 5100
+   #if (_WIZCHIP_ == 5200 || _WIZCHIP_ == 5500)
       case CW_SET_INTRTIME:
          setINTLEVEL(*(uint16_t*)arg);
          break;
@@ -228,6 +337,7 @@ int8_t ctlwizchip(ctlwizchip_type cwtype, void* arg)
       case CW_SET_PHYPOWMODE:
          return wizphy_setphypmode(*(uint8_t*)arg);
    #endif
+   #if _WIZCHIP_ == 5200 || _WIZCHIP_ == 5500
       case CW_GET_PHYPOWMODE:
          tmp = wizphy_getphypmode();
          if((int8_t)tmp == -1) return -1;
@@ -238,6 +348,7 @@ int8_t ctlwizchip(ctlwizchip_type cwtype, void* arg)
          if((int8_t)tmp == -1) return -1;
          *(uint8_t*)arg = tmp;
          break;
+   #endif      
       default:
          return -1;
    }
@@ -277,10 +388,21 @@ void wizchip_sw_reset(void)
 {
    uint8_t gw[4], sn[4], sip[4];
    uint8_t mac[6];
+//A20150601
+#if _WIZCHIP_IO_MODE_  == _WIZCHIP_IO_MODE_BUS_INDIR_
+   uint16_t mr = (uint16_t)getMR();
+   setMR(mr | MR_IND);
+#endif
+//
    getSHAR(mac);
    getGAR(gw);  getSUBR(sn);  getSIPR(sip);
    setMR(MR_RST);
    getMR(); // for delay
+//A2015051 : For indirect bus mode 
+#if _WIZCHIP_IO_MODE_  == _WIZCHIP_IO_MODE_BUS_INDIR_
+   setMR(mr | MR_IND);
+#endif
+//
    setSHAR(mac);
    setGAR(gw);
    setSUBR(sn);
@@ -295,18 +417,44 @@ int8_t wizchip_init(uint8_t* txsize, uint8_t* rxsize)
    if(txsize)
    {
       tmp = 0;
+   //M20150601 : For integrating with W5300
+   #if _WIZCHIP_ == 5300
       for(i = 0 ; i < _WIZCHIP_SOCK_NUM_; i++)
+      {
+         if(txsize[i] >= 64) return -1;   //No use 64KB even if W5300 support max 64KB memory allocation
          tmp += txsize[i];
-      if(tmp > 16) return -1;
+         if(tmp > 128) return -1;
+      }
+      if(tmp % 8) return -1;
+   #else      
+      for(i = 0 ; i < _WIZCHIP_SOCK_NUM_; i++)
+      {
+         tmp += txsize[i];
+         if(tmp > 16) return -1;
+      }
+   #endif
       for(i = 0 ; i < _WIZCHIP_SOCK_NUM_; i++)
          setSn_TXBUF_SIZE(i, txsize[i]);
    }
    if(rxsize)
    {
       tmp = 0;
+   #if _WIZCHIP_ == 5300
       for(i = 0 ; i < _WIZCHIP_SOCK_NUM_; i++)
+      {
+         if(rxsize[i] >= 64) return -1;   //No use 64KB even if W5300 support max 64KB memory allocation         
          tmp += rxsize[i];
-      if(tmp > 16) return -1;
+         if(tmp > 128) return -1;
+      }
+      if(tmp % 8) return -1;
+   #else         
+      for(i = 0 ; i < _WIZCHIP_SOCK_NUM_; i++)
+      {
+         tmp += rxsize[i];
+         if(tmp > 16) return -1;
+      }
+   #endif
+
       for(i = 0 ; i < _WIZCHIP_SOCK_NUM_; i++)
          setSn_RXBUF_SIZE(i, rxsize[i]);
    }
@@ -331,6 +479,9 @@ void wizchip_clrinterrupt(intr_kind intr)
 #if _WIZCHIP_ == 5100
    ir |= sir;
    setIR(ir);
+//A20150601 : For integrating with W5300
+#elif _WIZCHIP_ == 5300
+   setIR( ((((uint16_t)ir) << 8) | (((uint16_t)sir) & 0x00FF)) );
 #else
    setIR(ir);
    setSIR(sir);
@@ -344,13 +495,20 @@ intr_kind wizchip_getinterrupt(void)
    uint16_t ret = 0;
 #if _WIZCHIP_ == 5100
    ir = getIR();
-   sir = ir 0x0F;
+   sir = ir & 0x0F;
+//A20150601 : For integrating with W5300
+#elif _WIZCHIP_  == 5300
+   ret = getIR();
+   ir = (uint8_t)(ret >> 8);
+   sir = (uint8_t)ret;
 #else
    ir  = getIR();
    sir = getSIR();
 #endif         
 
-#if _WIZCHIP_ < 5500
+//M20150601 : For Integrating with W5300
+//#if _WIZCHIP_ < 5500
+#if _WIZCHIP_ < 5200
    ir &= ~(1<<4); // IK_WOL
 #endif
 #if _WIZCHIP_ == 5200
@@ -372,13 +530,13 @@ void wizchip_setinterruptmask(intr_kind intr)
    imr &= ~(1 << 6);
 #endif
    
-#if _WIZCHIP_ < 5200
-   simr &= 0x0F;
-#endif
-
 #if _WIZCHIP_ == 5100
+   simr &= 0x0F;
    imr |= simr;
    setIMR(imr);
+//A20150601 : For integrating with W5300
+#elif _WIZCHIP_ == 5300
+   setIMR( ((((uint16_t)imr) << 8) | (((uint16_t)simr) & 0x00FF)) );
 #else
    setIMR(imr);
    setSIMR(simr);
@@ -392,7 +550,12 @@ intr_kind wizchip_getinterruptmask(void)
    uint16_t ret = 0;
 #if _WIZCHIP_ == 5100
    imr  = getIMR();
-   simr = imr 0x0F;
+   simr = imr & 0x0F;
+//A20150601 : For integrating with W5300
+#elif _WIZCHIP_ == 5300
+   ret = getIMR();
+   imr = (uint8_t)(ret >> 8);
+   simr = (uint8_t)ret;
 #else
    imr  = getIMR();
    simr = getSIMR();
