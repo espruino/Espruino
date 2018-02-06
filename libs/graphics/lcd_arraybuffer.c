173ea21937d60b5c996a0e7bb21eda78231d2345
Gordon Williams <gw@pur3.co.uk>
2018-02-06 12:14:36 +0000

    improve arraybuffer fill speed

--- libs/graphics/lcd_arraybuffer.c
+++ libs/graphics/lcd_arraybuffer.c
@@ -54,65 +54,70 @@ unsigned int lcdGetPixel_ArrayBuffer(JsGraphics *gfx, short x, short y) {
 }
 
 // set pixelCount pixels starting at x,y
-void lcdSetPixels_ArrayBuffer(JsGraphics *gfx, short x, short y, short pixelCount, unsigned int col) {
-  JsVar *buf = jsvObjectGetChild(gfx->graphicsVar, "buffer", 0);
-  if (buf && jsvIsArrayBuffer(buf)) {
-    unsigned int idx = lcdGetPixelIndex_ArrayBuffer(gfx,x,y,pixelCount);
-    JsvArrayBufferIterator it;
-    jsvArrayBufferIteratorNew(&it, buf, idx>>3 );
+void lcdSetPixels_ArrayBuffer(JsGraphics *gfx, JsVar *buf, short x, short y, short pixelCount, unsigned int col) {
+  unsigned int idx = lcdGetPixelIndex_ArrayBuffer(gfx,x,y,pixelCount);
+  JsvArrayBufferIterator it;
+  jsvArrayBufferIteratorNew(&it, buf, idx>>3 );
 
-    unsigned int whiteMask = (1U<<gfx->data.bpp)-1;
-    bool shortCut = (col==0 || (col&whiteMask)==whiteMask) && (!(gfx->data.flags&JSGRAPHICSFLAGS_ARRAYBUFFER_VERTICAL_BYTE)); // simple black or white fill
+  unsigned int whiteMask = (1U<<gfx->data.bpp)-1;
+  bool shortCut = (col==0 || (col&whiteMask)==whiteMask) && (!(gfx->data.flags&JSGRAPHICSFLAGS_ARRAYBUFFER_VERTICAL_BYTE)); // simple black or white fill
 
-    while (pixelCount--) { // writing individual bits
-      if (gfx->data.bpp&7/*not a multiple of one byte*/) {
-        idx = idx & 7;
-        if (shortCut && idx==0) {
-          // Basically, if we're aligned and we're filling all 0 or all 1
-          // then we can go really quickly and can just fill
-          int wholeBytes = (gfx->data.bpp*(pixelCount+1)) >> 3;
-          if (wholeBytes) {
-            char c = (char)(col?0xFF:0);
-            pixelCount = (short)(pixelCount+1 - (wholeBytes*8/gfx->data.bpp));
-            while (wholeBytes--) {
-              jsvArrayBufferIteratorSetByteValue(&it,  c);
-              jsvArrayBufferIteratorNext(&it);
-            }
-            continue;
+  while (pixelCount--) { // writing individual bits
+    if (gfx->data.bpp&7/*not a multiple of one byte*/) {
+      idx = idx & 7;
+      if (shortCut && idx==0) {
+        // Basically, if we're aligned and we're filling all 0 or all 1
+        // then we can go really quickly and can just fill
+        int wholeBytes = (gfx->data.bpp*(pixelCount+1)) >> 3;
+        if (wholeBytes) {
+          char c = (char)(col?0xFF:0);
+          pixelCount = (short)(pixelCount+1 - (wholeBytes*8/gfx->data.bpp));
+          while (wholeBytes--) {
+            jsvArrayBufferIteratorSetByteValue(&it,  c);
+            jsvArrayBufferIteratorNext(&it);
           }
-        }
-        unsigned int mask = (unsigned int)(1<<gfx->data.bpp)-1;
-        unsigned int existing = (unsigned int)jsvArrayBufferIteratorGetIntegerValue(&it);
-        unsigned int bitIdx = (gfx->data.flags & JSGRAPHICSFLAGS_ARRAYBUFFER_MSB) ? 8-(idx+gfx->data.bpp) : idx;
-        jsvArrayBufferIteratorSetByteValue(&it, (char)((existing&~(mask<<bitIdx)) | ((col&mask)<<bitIdx)));
-        if (gfx->data.flags & JSGRAPHICSFLAGS_ARRAYBUFFER_VERTICAL_BYTE) {
-          jsvArrayBufferIteratorNext(&it);
-        } else {
-          idx += gfx->data.bpp;
-          if (idx>=8) jsvArrayBufferIteratorNext(&it);
-        }
-      } else { // we're writing whole bytes
-        int i;
-        for (i=0;i<gfx->data.bpp;i+=8) {
-          jsvArrayBufferIteratorSetByteValue(&it, (char)(col >> i));
-          jsvArrayBufferIteratorNext(&it);
+          continue;
         }
       }
+      unsigned int mask = (unsigned int)(1<<gfx->data.bpp)-1;
+      unsigned int existing = (unsigned int)jsvArrayBufferIteratorGetIntegerValue(&it);
+      unsigned int bitIdx = (gfx->data.flags & JSGRAPHICSFLAGS_ARRAYBUFFER_MSB) ? 8-(idx+gfx->data.bpp) : idx;
+      jsvArrayBufferIteratorSetByteValue(&it, (char)((existing&~(mask<<bitIdx)) | ((col&mask)<<bitIdx)));
+      if (gfx->data.flags & JSGRAPHICSFLAGS_ARRAYBUFFER_VERTICAL_BYTE) {
+        jsvArrayBufferIteratorNext(&it);
+      } else {
+        idx += gfx->data.bpp;
+        if (idx>=8) jsvArrayBufferIteratorNext(&it);
+      }
+    } else { // we're writing whole bytes
+      int i;
+      for (i=0;i<gfx->data.bpp;i+=8) {
+        jsvArrayBufferIteratorSetByteValue(&it, (char)(col >> i));
+        jsvArrayBufferIteratorNext(&it);
+      }
     }
-    jsvArrayBufferIteratorFree(&it);
+  }
+  jsvArrayBufferIteratorFree(&it);
+}
+
+void lcdSetPixel_ArrayBuffer(JsGraphics *gfx, short x, short y, unsigned int col) {
+  JsVar *buf = jsvObjectGetChild(gfx->graphicsVar, "buffer", 0);
+  if (buf && jsvIsArrayBuffer(buf)) {
+    lcdSetPixels_ArrayBuffer(gfx, buf, x, y, 1, col);
   }
   jsvUnLock(buf);
 }
 
-
-void lcdSetPixel_ArrayBuffer(JsGraphics *gfx, short x, short y, unsigned int col) {
-  lcdSetPixels_ArrayBuffer(gfx,x,y,1,col);
-}
-
 void  lcdFillRect_ArrayBuffer(struct JsGraphics *gfx, short x1, short y1, short x2, short y2) {
-  short y;
-  for (y=y1;y<=y2;y++)
-    lcdSetPixels_ArrayBuffer(gfx, x1, y, (short)(1+x2-x1), gfx->data.fgColor);
+  JsVar *buf = jsvObjectGetChild(gfx->graphicsVar, "buffer", 0);
+  if (buf && jsvIsArrayBuffer(buf)) {
+    short y;
+    for (y=y1;y<=y2;y++)
+      lcdSetPixels_ArrayBuffer(gfx, buf, x1, y, (short)(1+x2-x1), gfx->data.fgColor);
+  }
+  jsvUnLock(buf);
+
+
 }
 
 void lcdInit_ArrayBuffer(JsGraphics *gfx) {
