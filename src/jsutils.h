6a01f1df3de46fd12d711e12703cb7fcf8617f0e
Gordon Williams <gw@pur3.co.uk>
2018-02-21 15:18:04 +0000

    1v96 release

--- src/jsutils.h
+++ src/jsutils.h
@@ -26,9 +26,9 @@
 #include <math.h>
 
 #ifndef BUILDNUMBER
-#define JS_VERSION "1v95"
+#define JS_VERSION "1v96"
 #else
-#define JS_VERSION "1v95." BUILDNUMBER
+#define JS_VERSION "1v96." BUILDNUMBER
 #endif
 /*
   In code:
