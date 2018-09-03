espruino/Espruino

commit 9a15ae0a831d2fa48bc36abf2d02cb395a95044b

Add jsvAddFunctionParameter and remove some code duplication

src/jswrap_functions.c
@@ -87,13 +87,7 @@ JsVar *jswrap_function_constructor(JsVar *args) {
     if (s) {
       // copy the string - if a string was supplied already we want to make
       // sure we have a new (unreferenced) string
-      JsVar *paramName = jsvNewFromString("\xFF");
-      if (paramName) {
-        jsvAppendStringVarComplete(paramName, s);
-        jsvMakeFunctionParameter(paramName); // force this to be called a function parameter
-        jsvAddName(fn, paramName);
-        jsvUnLock(paramName);
-      }
+      jsvAddFunctionParameter(fn, jsvNewFromString("\xFF"), 0);
       jsvUnLock(s);
     }
 

src/jsparse.c
@@ -623,14 +623,7 @@ NO_INLINE JsVar *jspeFunctionCall(JsVar *function, JsVar *functionName, JsVar *t
       JsVar *param = jsvObjectIteratorGetKey(&it);
       JsVar *value = jsvObjectIteratorGetValue(&it);
       while (jsvIsFunctionParameter(param) && value) {
-        JsVar *paramName = jsvNewFromStringVar(param,1,JSVAPPENDSTRINGVAR_MAXLENGTH);
-        if (paramName) { // could be out of memory
-          jsvMakeFunctionParameter(paramName); // force this to be called a function parameter
-          jsvSetValueOfName(paramName, value);
-          jsvAddName(functionRoot, paramName);
-          jsvUnLock(paramName);
-        } else
-          jspSetError(false);
+        jsvAddFunctionParameter(functionRoot, jsvNewFromStringVar(param,1,JSVAPPENDSTRINGVAR_MAXLENGTH), value);
         jsvUnLock2(value, param);
         jsvObjectIteratorNext(&it);
         param = jsvObjectIteratorGetKey(&it);
@@ -652,14 +645,7 @@ NO_INLINE JsVar *jspeFunctionCall(JsVar *function, JsVar *functionName, JsVar *t
               value = jspeAssignmentExpression();
             // and if execute, copy it over
             value = jsvSkipNameAndUnLock(value);
-            JsVar *paramName = paramDefined ? jsvNewFromStringVar(param,1,JSVAPPENDSTRINGVAR_MAXLENGTH) : jsvNewFromEmptyString();
-            if (paramName) { // could be out of memory
-              jsvMakeFunctionParameter(paramName); // force this to be called a function parameter
-              jsvSetValueOfName(paramName, value);
-              jsvAddName(functionRoot, paramName);
-              jsvUnLock(paramName);
-            } else
-              jspSetError(false);
+            jsvAddFunctionParameter(functionRoot, paramDefined?jsvNewFromStringVar(param,1,JSVAPPENDSTRINGVAR_MAXLENGTH):0, value);
             jsvUnLock(value);
             if (lex->tk!=')') JSP_MATCH(',');
           }
@@ -672,14 +658,7 @@ NO_INLINE JsVar *jspeFunctionCall(JsVar *function, JsVar *functionName, JsVar *t
         while (args<argCount) {
           JsVar *param = jsvObjectIteratorGetKey(&it);
           bool paramDefined = jsvIsFunctionParameter(param);
-          JsVar *paramName = paramDefined ? jsvNewFromStringVar(param,1,JSVAPPENDSTRINGVAR_MAXLENGTH) : jsvNewFromEmptyString();
-          if (paramName) {
-            jsvMakeFunctionParameter(paramName); // force this to be called a function parameter
-            jsvSetValueOfName(paramName, argPtr[args]);
-            jsvAddName(functionRoot, paramName);
-            jsvUnLock(paramName);
-          } else
-            jspSetError(false);
+          jsvAddFunctionParameter(functionRoot, paramDefined?jsvNewFromStringVar(param,1,JSVAPPENDSTRINGVAR_MAXLENGTH):0, argPtr[args]);
           args++;
           jsvUnLock(param);
           if (paramDefined) jsvObjectIteratorNext(&it);
@@ -697,15 +676,9 @@ NO_INLINE JsVar *jspeFunctionCall(JsVar *function, JsVar *functionName, JsVar *t
             thisVar = jsvSkipName(param);
           } else if (jsvIsStringEqual(param, JSPARSE_FUNCTION_LINENUMBER_NAME)) functionLineNumber = (uint16_t)jsvGetIntegerAndUnLock(jsvSkipName(param));
           else if (jsvIsFunctionParameter(param)) {
-            JsVar *paramName = jsvNewFromStringVar(param,1,JSVAPPENDSTRINGVAR_MAXLENGTH);
-            // paramName is already a name (it's a function parameter)
-            if (paramName) {// could be out of memory - or maybe just not supplied!
-              jsvMakeFunctionParameter(paramName);
-              JsVar *defaultVal = jsvSkipName(param);
-              if (defaultVal) jsvUnLock(jsvSetValueOfName(paramName, defaultVal));
-              jsvAddName(functionRoot, paramName);
-              jsvUnLock(paramName);
-            }
+            JsVar *defaultVal = jsvSkipName(param);
+            jsvAddFunctionParameter(functionRoot, jsvNewFromStringVar(param,1,JSVAPPENDSTRINGVAR_MAXLENGTH), defaultVal);
+            jsvUnLock(defaultVal);
           }
         }
         jsvUnLock(param);

src/jsvar.c
@@ -961,6 +961,60 @@ JsVar *jsvNewFromLongInteger(long long value) {
     return jsvNewFromFloat((JsVarFloat)value);
 }
 
+JsVar *jsvNewFromPin(int pin) {
+  JsVar *v = jsvNewFromInteger((JsVarInt)pin);
+  if (v) {
+    v->flags = (JsVarFlags)((v->flags & ~JSV_VARTYPEMASK) | JSV_PIN);
+  }
+  return v;
+}
+
+JsVar *jsvNewObject() {
+  return jsvNewWithFlags(JSV_OBJECT);
+}
+
+JsVar *jsvNewEmptyArray() {
+  return jsvNewWithFlags(JSV_ARRAY);
+}
+
+/// Create an array containing the given elements
+JsVar *jsvNewArray(JsVar **elements, int elementCount) {
+  JsVar *arr = jsvNewEmptyArray();
+  if (!arr) return 0;
+  int i;
+  for (i=0;i<elementCount;i++)
+    jsvArrayPush(arr, elements[i]);
+  return arr;
+}
+
+JsVar *jsvNewNativeFunction(void (*ptr)(void), unsigned short argTypes) {
+  JsVar *func = jsvNewWithFlags(JSV_FUNCTION | JSV_NATIVE);
+  if (!func) return 0;
+  func->varData.native.ptr = ptr;
+  func->varData.native.argTypes = argTypes;
+  return func;
+}
+
+JsVar *jsvNewNativeString(char *ptr, size_t len) {
+  if (len>JSV_NATIVE_STR_MAX_LENGTH) len=JSV_NATIVE_STR_MAX_LENGTH; // crop string to what we can store in nativeStr.len
+  JsVar *str = jsvNewWithFlags(JSV_NATIVE_STRING);
+  if (!str) return 0;
+  str->varData.nativeStr.ptr = ptr;
+  str->varData.nativeStr.len = (uint16_t)len;
+  return str;
+}
+
+/// Create a new ArrayBuffer backed by the given string. If length is not specified, it will be worked out
+JsVar *jsvNewArrayBufferFromString(JsVar *str, unsigned int lengthOrZero) {
+  JsVar *arr = jsvNewWithFlags(JSV_ARRAYBUFFER);
+  if (!arr) return 0;
+  jsvSetFirstChild(arr, jsvGetRef(jsvRef(str)));
+  arr->varData.arraybuffer.type = ARRAYBUFFERVIEW_ARRAYBUFFER;
+  assert(arr->varData.arraybuffer.byteOffset == 0);
+  if (lengthOrZero==0) lengthOrZero = (unsigned int)jsvGetStringLength(str);
+  arr->varData.arraybuffer.length = (unsigned short)lengthOrZero;
+  return arr;
+}
 
 JsVar *jsvMakeIntoVariableName(JsVar *var, JsVar *valueOrZero) {
   if (!var) return 0;
@@ -1047,47 +1101,17 @@ void jsvMakeFunctionParameter(JsVar *v) {
   v->flags = (JsVarFlags)(v->flags | JSV_NATIVE);
 }
 
-JsVar *jsvNewFromPin(int pin) {
-  JsVar *v = jsvNewFromInteger((JsVarInt)pin);
-  if (v) {
-    v->flags = (JsVarFlags)((v->flags & ~JSV_VARTYPEMASK) | JSV_PIN);
+/// Add a new unnamed function parameter to a function - use this when binding function arguments. This unlocks paramName if specified, but not value.
+void jsvAddFunctionParameter(JsVar *fn, JsVar *paramName, JsVar *value) {
+  assert(jsvIsFunction(fn));
+  if (!paramName) paramName = jsvNewFromEmptyString();
+  assert(jsvIsString(paramName));
+  if (paramName) {
+    jsvMakeFunctionParameter(paramName); // force this to be called a function parameter
+    jsvSetValueOfName(paramName, value);
+    jsvAddName(fn, paramName);
+    jsvUnLock(paramName);
   }
-  return v;
-}
-
-JsVar *jsvNewObject() {
-  return jsvNewWithFlags(JSV_OBJECT);
-}
-
-JsVar *jsvNewEmptyArray() {
-  return jsvNewWithFlags(JSV_ARRAY);
-}
-
-/// Create an array containing the given elements
-JsVar *jsvNewArray(JsVar **elements, int elementCount) {
-  JsVar *arr = jsvNewEmptyArray();
-  if (!arr) return 0;
-  int i;
-  for (i=0;i<elementCount;i++)
-    jsvArrayPush(arr, elements[i]);
-  return arr;
-}
-
-JsVar *jsvNewNativeFunction(void (*ptr)(void), unsigned short argTypes) {
-  JsVar *func = jsvNewWithFlags(JSV_FUNCTION | JSV_NATIVE);
-  if (!func) return 0;
-  func->varData.native.ptr = ptr;
-  func->varData.native.argTypes = argTypes;
-  return func;
-}
-
-JsVar *jsvNewNativeString(char *ptr, size_t len) {
-  if (len>JSV_NATIVE_STR_MAX_LENGTH) len=JSV_NATIVE_STR_MAX_LENGTH; // crop string to what we can store in nativeStr.len
-  JsVar *str = jsvNewWithFlags(JSV_NATIVE_STRING);
-  if (!str) return 0;
-  str->varData.nativeStr.ptr = ptr;
-  str->varData.nativeStr.len = (uint16_t)len;
-  return str;
 }
 
 void *jsvGetNativeFunctionPtr(const JsVar *function) {
@@ -1103,17 +1127,6 @@ void *jsvGetNativeFunctionPtr(const JsVar *function) {
     return (void *)function->varData.native.ptr;
 }
 
-/// Create a new ArrayBuffer backed by the given string. If length is not specified, it will be worked out
-JsVar *jsvNewArrayBufferFromString(JsVar *str, unsigned int lengthOrZero) {
-  JsVar *arr = jsvNewWithFlags(JSV_ARRAYBUFFER);
-  if (!arr) return 0;
-  jsvSetFirstChild(arr, jsvGetRef(jsvRef(str)));
-  arr->varData.arraybuffer.type = ARRAYBUFFERVIEW_ARRAYBUFFER;
-  assert(arr->varData.arraybuffer.byteOffset == 0);
-  if (lengthOrZero==0) lengthOrZero = (unsigned int)jsvGetStringLength(str);
-  arr->varData.arraybuffer.length = (unsigned short)lengthOrZero;
-  return arr;
-}
 
 bool jsvIsBasicVarEqual(JsVar *a, JsVar *b) {
   // quick checks

src/jsvar.h
@@ -323,11 +323,8 @@ JsVar *jsvNewFromStringVar(const JsVar *str, size_t stridx, size_t maxLength);
 JsVar *jsvNewFromInteger(JsVarInt value);
 JsVar *jsvNewFromBool(bool value);
 JsVar *jsvNewFromFloat(JsVarFloat value);
-// Create an integer (or float) from this value, depending on whether it'll fit in 32 bits or not.
+/// Create an integer (or float) from this value, depending on whether it'll fit in 32 bits or not.
 JsVar *jsvNewFromLongInteger(long long value);
-// Turns var into a Variable name that links to the given value... No locking so no need to unlock var
-JsVar *jsvMakeIntoVariableName(JsVar *var, JsVar *valueOrZero);
-void jsvMakeFunctionParameter(JsVar *v);
 JsVar *jsvNewFromPin(int pin);
 JsVar *jsvNewObject(); ///< Create a new object
 JsVar *jsvNewEmptyArray(); ///< Create a new array
@@ -335,6 +332,13 @@ JsVar *jsvNewArray(JsVar **elements, int elementCount); ///< Create an array con
 JsVar *jsvNewNativeFunction(void (*ptr)(void), unsigned short argTypes); ///< Create an array containing the given elements
 JsVar *jsvNewNativeString(char *ptr, size_t len); ///< Create a Native String pointing to the given memory area
 JsVar *jsvNewArrayBufferFromString(JsVar *str, unsigned int lengthOrZero); ///< Create a new ArrayBuffer backed by the given string. If length is not specified, it will be worked out
+
+/// Turns var into a Variable name that links to the given value... No locking so no need to unlock var
+JsVar *jsvMakeIntoVariableName(JsVar *var, JsVar *valueOrZero);
+/// Turns var into a 'function parameter' that the parser recognises when parsing a function
+void jsvMakeFunctionParameter(JsVar *v);
+/// Add a new function parameter to a function (name may be 0) - use this when binding function arguments This unlocks paramName if specified, but not value.
+void jsvAddFunctionParameter(JsVar *fn, JsVar *name, JsVar *value);
 
 void *jsvGetNativeFunctionPtr(const JsVar *function); ///< Get the actual pointer from a native function - this may not be the contents of varData.native.ptr
 

src/jswrap_object.c
@@ -1049,13 +1049,7 @@ JsVar *jswrap_function_bind(JsVar *parent, JsVar *thisArg, JsVar *argsArray) {
       }
 
       if (!addedParam) {
-        JsVar *paramName = jsvNewFromEmptyString();
-        if (paramName) {
-          jsvMakeFunctionParameter(paramName); // force this to be called a function parameter
-          jsvSetValueOfName(paramName, defaultValue);
-          jsvAddName(fn, paramName);
-          jsvUnLock(paramName);
-        }
+        jsvAddFunctionParameter(fn, 0, defaultValue);
       }
       jsvUnLock(defaultValue);
       jsvObjectIteratorNext(&argIt);

src/jswrap_promise.c
@@ -268,14 +268,9 @@ JsVar *jswrap_promise_all(JsVar *arr) {
       if (_jswrap_promise_is_promise(p)) {
         JsVar *resolve = jsvNewNativeFunction((void (*)(void))jswrap_promise_all_resolve, JSWAT_VOID|JSWAT_THIS_ARG|(JSWAT_INT32<<JSWAT_BITS)|(JSWAT_JSVAR<<(JSWAT_BITS*2)));
         // bind the index variable
-        JsVar *paramName = jsvNewFromEmptyString();
-        if (paramName) {
-          jsvMakeFunctionParameter(paramName); // force this to be called a function parameter
-          JsVar *indexVar = jsvNewFromInteger(promiseIndex);
-          jsvSetValueOfName(paramName, indexVar);
-          jsvAddName(resolve, paramName);
-          jsvUnLock2(indexVar, paramName);
-        }
+        JsVar *indexVar = jsvNewFromInteger(promiseIndex);
+        jsvAddFunctionParameter(resolve, 0, indexVar);
+        jsvUnLock(indexVar);
         jsvObjectSetChild(resolve, JSPARSE_FUNCTION_THIS_NAME, promise); // bind 'this'
         jsvUnLock2(jswrap_promise_then(p, resolve, reject), resolve);
       } else {

commit 737dab0f39fedc6124bd9bb43c9869b4d0864a0c

fix 'new Function' regression after 9a15ae0a831d2fa48bc36abf2d02cb395a95044b

src/jswrap_functions.c
@@ -87,7 +87,11 @@ JsVar *jswrap_function_constructor(JsVar *args) {
     if (s) {
       // copy the string - if a string was supplied already we want to make
       // sure we have a new (unreferenced) string
-      jsvAddFunctionParameter(fn, jsvNewFromString("\xFF"), 0);
+      JsVar *paramName = jsvNewFromString("\xFF");
+      if (paramName) {
+        jsvAppendStringVarComplete(paramName, s);
+        jsvAddFunctionParameter(fn, paramName, 0);
+      }
       jsvUnLock(s);
     }
 