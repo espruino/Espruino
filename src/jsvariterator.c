espruino/Espruino

commit 0960a88376c37f60884f9a4e6f39c55e33e88581

Improve fast path when iterating over Uint8Array/ArrayBuffer

src/jsvariterator.c
@@ -61,10 +61,11 @@ bool jsvIterateCallback(
     JsvArrayBufferIterator it;
     jsvArrayBufferIteratorNew(&it, data, 0);
     if (JSV_ARRAYBUFFER_GET_SIZE(it.type) == 1 && !JSV_ARRAYBUFFER_IS_SIGNED(it.type)) {
-      // faster for single byte arrays.
-      while (jsvArrayBufferIteratorHasElement(&it)) {
-        callback((int)(unsigned char)jsvStringIteratorGetChar(&it.it), callbackData);
-        jsvArrayBufferIteratorNext(&it);
+      JsvStringIterator *sit = &it.it;
+      // faster for single byte arrays - read using the string iterator.
+      while (jsvStringIteratorHasChar(sit)) {
+        callback((int)(unsigned char)jsvStringIteratorGetChar(sit), callbackData);
+        jsvStringIteratorNextInline(sit);
       }
     } else {
       while (jsvArrayBufferIteratorHasElement(&it)) {

commit 31815652628ae2189f26a10949b6dac42e812efd

fix invalid extra iterations caused by recent 0960a88376c37f60884f9a4e6f39c55e33e88581

src/jsvariterator.c
@@ -13,6 +13,7 @@
  */
 #include "jsvariterator.h"
 #include "jsparse.h"
+#include "jsinteractive.h"
 
 /**
  Iterate over the contents of the content of a variable, calling callback for each.
@@ -78,7 +79,8 @@ bool jsvIterateCallback(
     if (JSV_ARRAYBUFFER_GET_SIZE(it.type) == 1 && !JSV_ARRAYBUFFER_IS_SIGNED(it.type)) {
       JsvStringIterator *sit = &it.it;
       // faster for single byte arrays - read using the string iterator.
-      while (jsvStringIteratorHasChar(sit)) {
+      size_t len = jsvGetArrayBufferLength(data);
+      while (len--) {
         callback((int)(unsigned char)jsvStringIteratorGetChar(sit), callbackData);
         jsvStringIteratorNextInline(sit);
       }