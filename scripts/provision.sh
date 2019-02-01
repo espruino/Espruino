8088a57c9f2dbb2f361c77d3551b84987ab81699
wilberforce <github@wilberforce.co.nz>
2019-02-01 20:32:32 +1300

    Update provision.sh

--- scripts/provision.sh
+++ scripts/provision.sh
@@ -72,12 +72,12 @@ if [ "$PROVISION_ESP32" = "1" ]; then
     if [ ! -d "app" ]; then
         echo installing app folder
         #curl -Ls https://github.com/espruino/EspruinoBuildTools/raw/master/esp32/deploy/app.tgz | tar xfz - --no-same-owner
-        curl -Ls https://github.com/espruino/EspruinoBuildTools/raw/ESP32-V3.1/esp32/deploy/app.tgz | tar xfz - --no-same-owner
+        curl -Ls https://github.com/espruino/EspruinoBuildTools/raw/v3.1.2/esp32/deploy/app.tgz | tar xfz - --no-same-owner
     fi
     if [ ! -d "esp-idf" ]; then
         echo installing esp-idf folder
         #curl -Ls https://github.com/espruino/EspruinoBuildTools/raw/master/esp32/deploy/esp-idf.tgz | tar xfz - --no-same-owner
-        curl -Ls https://github.com/espruino/EspruinoBuildTools/raw/ESP32-V3.1/esp32/deploy/esp-idf.tgz | tar xfz - --no-same-owner
+        curl -Ls https://github.com/espruino/EspruinoBuildTools/raw/v3.1.2/esp32/deploy/esp-idf.tgz | tar xfz - --no-same-owner
     fi
     if ! type xtensa-esp32-elf-gcc 2> /dev/null > /dev/null; then
         echo installing xtensa-esp32-elf-gcc
