ed784851083eafef29de50b818d693b71a4a2d18
MaBecker <mark.becker@sbit-gmbh.de>
2019-03-07 21:26:58 +0100

    add brackets to logical expression (fix #1629)

--- tests/test_array_access.js
+++ tests/test_array_access.js
@@ -1,4 +1,4 @@
 // https://github.com/espruino/Espruino/issues/290
 var arr = ["a"];
-result = ((arr[true] === undefined) === true) &&
-          ((arr[false] === undefined) === true);
+result = (((arr[true] === undefined) === true) &&
+          ((arr[false] === undefined) === true));
