a79ec1146f26ec48b54d6d4ecb459094e9506ad3
Gordon Williams <gw@pur3.co.uk>
2019-05-01 10:11:50 +0100

    2v02 release

--- src/jsutils.h
+++ src/jsutils.h
@@ -26,9 +26,9 @@
 #include <math.h>
 
 #ifndef BUILDNUMBER
-#define JS_VERSION "2v01"
+#define JS_VERSION "2v02"
 #else
-#define JS_VERSION "2v01." BUILDNUMBER
+#define JS_VERSION "2v02." BUILDNUMBER
 #endif
 /*
   In code:
