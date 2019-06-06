8708a2270708967cc9cfcd9d4d5415d8ff536c25
Gordon Williams <gw@pur3.co.uk>
2019-06-06 17:06:47 +0100

    initial ID205 commit

---
+++ libs/id205/jswrap_id205.h
@@ -0,0 +1,22 @@
+/*
+ * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+ *
+ * Copyright (C) 2016 Gordon Williams <gw@pur3.co.uk>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ----------------------------------------------------------------------------
+* Contains JavaScript interface for Pixl.js (http://www.espruino.com/Pixl.js)
+ * ----------------------------------------------------------------------------
+ */
+#include "jspin.h"
+
+void jswrap_id205_lcdw(JsVarInt c);
+void jswrap_id205_setContrast(JsVarFloat c);
+void jswrap_id205_setLCDPower(bool isOn);
+
+void jswrap_id205_init();
+void jswrap_id205_kill();
+bool jswrap_id205_idle();
