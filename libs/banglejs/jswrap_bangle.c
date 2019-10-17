d208ffd756825cf3c04da39151d1e1fa0e954292
Gordon Williams <gw@pur3.co.uk>
2019-10-17 17:25:52 +0100

    More work on tap detect - nothing yet

--- libs/banglejs/jswrap_bangle.c
+++ libs/banglejs/jswrap_bangle.c
@@ -138,7 +138,7 @@ Has the screen been turned on or off? Can be used to stop tasks that are no long
 /*JSON{
   "type" : "event",
   "class" : "Bangle",
-  "name" : "faceUp",
+  "name" : "tap",
   "params" : [["data","JsVar","`{dir, double, x, y, z}`"]],
   "ifdef" : "BANGLEJS"
 }
@@ -336,6 +336,8 @@ void peripheralPollHandler() {
     // report tap
     tapInfo = buf[0] | (tapType<<6);
     bangleTasks |= JSBT_ACCEL_TAPPED;
+  }
+  if (tapType || hasAccelData) {
     // clear the IRQ flags
     buf[0]=0x17;
     jsi2cWrite(&internalI2C, ACCEL_ADDR, 1, buf, true);
@@ -744,7 +746,7 @@ void jswrap_banglejs_init() {
   jsvUnLock(graphics);
 
   // accelerometer init
-  jswrap_banglejs_accelWr(0x18,0x0a); // CNTL1 Off, low power, 4g range, Wakeup
+  jswrap_banglejs_accelWr(0x18,0x0a); // CNTL1 Off (top bit)
   jswrap_banglejs_accelWr(0x19,0x80); // CNTL2 Software reset
   jshDelayMicroseconds(2000);
   jswrap_banglejs_accelWr(0x1a,0b10011000); // CNTL3 12.5Hz tilt, 400Hz tap, 0.781Hz motion detection
@@ -759,14 +761,15 @@ void jswrap_banglejs_init() {
   jswrap_banglejs_accelWr(0x21,0); // INC6 disabled
   jswrap_banglejs_accelWr(0x23,3); // WUFC wakeupi detect counter
   jswrap_banglejs_accelWr(0x24,3); // TDTRC Tap detect enable
-  jswrap_banglejs_accelWr(0x25, 0x78); // TDTC Tap detect double tap
-  jswrap_banglejs_accelWr(0x26, 0x20); // TTH Tap detect threshold high (0xCB recommended)
-  jswrap_banglejs_accelWr(0x27, 0x10); // TTH Tap detect threshold low (0x1A recommended)
+  jswrap_banglejs_accelWr(0x25, 0x78); // TDTC Tap detect double tap (0x78 default)
+  jswrap_banglejs_accelWr(0x26, 0x65); // TTH Tap detect threshold high (0xCB default)
+  jswrap_banglejs_accelWr(0x27, 0x0D); // TTL Tap detect threshold low (0x1A default)
   jswrap_banglejs_accelWr(0x30,1); // ATH low wakeup detect threshold
   jswrap_banglejs_accelWr(0x35,0); // LP_CNTL no averaging of samples
   //jswrap_banglejs_accelWr(0x35,2); // LP_CNTL 4x averaging of samples
   jswrap_banglejs_accelWr(0x3e,0); // clear the buffer
-  jswrap_banglejs_accelWr(0x18,0b10101100);  // CNTL1 On, low power, DRDYE, ODR/2(high res), 4g range, Wakeup, TDTE (tap enable)
+  jswrap_banglejs_accelWr(0x18,0b01101100);  // CNTL1 Off, high power, DRDYE=1, 4g range, TDTE (tap enable)=1, Wakeup=0, Tilt=0
+  jswrap_banglejs_accelWr(0x18,0b11101100);  // CNTL1 On, high power, DRDYE=1, 4g range, TDTE (tap enable)=1, Wakeup=0, Tilt=0
   // compass init
   jswrap_banglejs_compassWr(0x32,1);
   jswrap_banglejs_compassWr(0x31,0);
