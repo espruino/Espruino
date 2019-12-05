be6fe41ed423caaef795248f1cec38340e77c1d3
Gordon Williams <gw@pur3.co.uk>
2019-12-05 10:38:04 +0000

    now flip when setting LED - turns display on

--- libs/js/banglejs/LED2.js
+++ libs/js/banglejs/LED2.js
@@ -4,7 +4,7 @@
     read: function() { return this._v; },
     set: function() { this.write(1); },
     reset: function() { this.write(0); },
-    write: function(v) { g.setColor(0,(this._v=!!v)?1:0,0).fillCircle(130,5,5); },
+    write: function(v) { g.setColor(0,(this._v=!!v)?1:0,0).fillCircle(130,5,5).flip(); },
     toggle: function() { this.write(!this._v); }
   };
 })
