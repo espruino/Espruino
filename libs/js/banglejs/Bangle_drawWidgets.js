13e58bd0027b591c6a77f025f11aba642ddac9f1
Gordon Williams <gw@pur3.co.uk>
2020-03-05 13:24:43 +0000

    Bangle.js: Modify handling of widgets to allow variable width widgets (requires new widget JS)

--- libs/js/banglejs/Bangle_drawWidgets.js
+++ libs/js/banglejs/Bangle_drawWidgets.js
@@ -1,3 +1,18 @@
 (function() {
-  if (global.WIDGETS) for (var w of WIDGETS) w.draw();
+  var w=g.getWidth(), h=g.getHeight();
+  var pos = {
+	  tl:{x:32, y:0, r:0}, // if r==1, we're right->left
+	  tr:{x:w-32, y:0, r:1},
+	  bl:{x:32, y:h-24, r:0},
+	  br:{x:w-32, y:h-24, r:1}
+  };
+  g.reset().clearRect(0,0,w-1,23).clearRect(0,h-24,w-1,h-1);
+  if (global.WIDGETS) for (var wd of WIDGETS) {
+	  var p = pos[wd.area];
+	  if (!p) return;
+	  wd.x = p.x - p.r*wd.width;
+	  wd.y = p.y;
+	  p.x += wd.width*(1-2*p.r);
+	  wd.draw(wd);
+  }
 })
--- libs/js/banglejs/Bangle_loadWidgets.js
+++ libs/js/banglejs/Bangle_loadWidgets.js
@@ -1,5 +1,4 @@
 (function() {
-  global.WIDGETPOS={tl:32,tr:g.getWidth()-32,bl:32,br:g.getWidth()-32};
   global.WIDGETS={};
   require("Storage").list(/\.wid\.js$/).forEach(widget=>eval(require("Storage").read(widget)));
 })
