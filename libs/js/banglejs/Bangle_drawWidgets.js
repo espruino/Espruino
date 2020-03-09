3182c0f071460bf75d65b6008dd5bcaed51088fe
Gordon Williams <gw@pur3.co.uk>
2020-03-09 14:42:52 +0000

    Bangle.js: Modify Bangle.drawWidgets to only clear the areas where widgets actually are, not the whole top&bottom bar (ref https://github.com/espruino/BangleApps/issues/121)

--- libs/js/banglejs/Bangle_drawWidgets.js
+++ libs/js/banglejs/Bangle_drawWidgets.js
@@ -1,18 +1,22 @@
 (function() {
   var w=g.getWidth(), h=g.getHeight();
   var pos = {
-	  tl:{x:32, y:0, r:0}, // if r==1, we're right->left
-	  tr:{x:w-32, y:0, r:1},
-	  bl:{x:32, y:h-24, r:0},
-	  br:{x:w-32, y:h-24, r:1}
+	  tl:{x:28, y:0, r:0}, // if r==1, we're right->left
+	  tr:{x:w-28, y:0, r:1},
+	  bl:{x:24, y:h-24, r:0},
+	  br:{x:w-24, y:h-24, r:1}
   };
-  g.reset().clearRect(0,0,w-1,23).clearRect(0,h-24,w-1,h-1);
-  if (global.WIDGETS) for (var wd of WIDGETS) {
+  if (global.WIDGETS) {
+    for (var wd of WIDGETS) {
 	  var p = pos[wd.area];
 	  if (!p) return;
 	  wd.x = p.x - p.r*wd.width;
 	  wd.y = p.y;
 	  p.x += wd.width*(1-2*p.r);
 	  wd.draw(wd);
+    }
+    g.reset().clearRect(0,0,pos.tl.x,23).clearRect(0,h-24,pos.bl.x,h-1); // left
+    g.clearRect(pos.tr.x,0,w-1,23).clearRect(pos.br.x,h-24,w-1,h-1); // right
+    for (wd of WIDGETS) wd.draw(wd);
   }
 })
