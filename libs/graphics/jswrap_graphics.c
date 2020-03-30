699d64efd39d73d3e5e804c280b38d7d187a04ce
Gordon Williams <gw@pur3.co.uk>
2020-03-30 15:02:45 +0100

    Adding docs for flip and buffer, fix https://github.com/espruino/EspruinoDocs/issues/545

--- libs/graphics/jswrap_graphics.c
+++ libs/graphics/jswrap_graphics.c
@@ -79,6 +79,62 @@ Use Graphics.createXXX to create a graphics object that renders in the way you w
 */
 
 /*JSON{
+  "type" : "method",
+  "class" : "Graphics",
+  "params" : [ ["all","bool","(only on some devices) If `true` then copy all pixels, not just those that have changed."] ],
+  "name" : "flip"
+}
+On instances of graphics that drive a display with
+an offscreen buffer, calling this function will
+copy the contents of the offscreen buffer to the
+screen.
+
+Call this when you have drawn something to Graphics
+and you want it shown on the screen.
+
+If a display does not have an offscreen buffer,
+it may not have a `g.flip()` method.
+
+On Bangle.js, there are different graphics modes
+chosen with `Bangle.setLCDMode()`. The default mode
+is unbuffered and in this mode `g.flip()` does not
+affect the screen contents, however it will cause the
+screen to light up if it was previously off due
+to inactivity.
+
+On some devices, this command will attempt to
+only update the areas of the screen that have
+changed in order to increase speed. If you have
+accessed the `Graphics.buffer` directly then you
+may need to use `Graphics.flip(true)` to force
+a full update of the screen.
+*/
+/*JSON{
+  "type" : "property",
+  "class" : "Graphics",
+  "name" : "buffer"
+}
+On Graphics instances with an offscreen buffer, this
+is an `ArrayBuffer` that provides access to the underlying
+pixel data.
+
+```
+g=Graphics.createArrayBuffer(8,8,8)
+g.drawLine(0,0,7,7)
+print(new Uint8Array(g.buffer))
+new Uint8Array([
+255, 0, 0, 0, 0, 0, 0, 0,
+0, 255, 0, 0, 0, 0, 0, 0,
+0, 0, 255, 0, 0, 0, 0, 0,
+0, 0, 0, 255, 0, 0, 0, 0,
+0, 0, 0, 0, 255, 0, 0, 0,
+0, 0, 0, 0, 0, 255, 0, 0,
+0, 0, 0, 0, 0, 0, 255, 0,
+0, 0, 0, 0, 0, 0, 0, 255])
+```
+*/
+
+/*JSON{
   "type" : "idle",
   "generate" : "jswrap_graphics_idle"
 }*/
