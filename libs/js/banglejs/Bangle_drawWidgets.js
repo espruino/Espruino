87d8b8cc722800dc20ebc5d74ca81d974ef80818
Gordon Williams <gw@pur3.co.uk>
2020-03-31 15:58:56 +0100

    Bangle.js: Now clear entire top/bottom row if there are widgets in it (fix #1790)

--- libs/js/banglejs/Bangle_drawWidgets.js
+++ libs/js/banglejs/Bangle_drawWidgets.js
@@ -14,13 +14,10 @@
 	  wd.y = p.y;
 	  p.x += wd.width*(1-2*p.r);
 	  p.c++;
-	  wd.draw(wd);
     }
     g.reset();
-    if (pos.tl.c) g.clearRect(0,0,pos.tl.x,23);
-    if (pos.bl.c) g.clearRect(0,h-24,pos.bl.x,h-1);
-    if (pos.tr.c) g.clearRect(pos.tr.x,0,w-1,23);
-    if (pos.br.c) g.clearRect(pos.br.x,h-24,w-1,h-1);
+    if (pos.tl.c || pos.tr.c) g.clearRect(0,0,w-1,23);
+    if (pos.bl.c || pos.br.c) g.clearRect(0,h-24,w-1,x,h-1);
     for (wd of WIDGETS) wd.draw(wd);
   }
 })
