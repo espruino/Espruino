41028995b26c03da13b619410aae1df27e9ada5c
Gordon Williams <gw@pur3.co.uk>
2020-04-29 11:17:48 +0100

    Pixl.js: Fix E.showMessage

--- libs/js/pixljs/E_showMessage.js
+++ libs/js/pixljs/E_showMessage.js
@@ -1,17 +1,17 @@
 (function(msg,title) {
   g.clear(1);
-  g.setFont("6x8",2);
+  g.setFont("4x6");
   g.setFontAlign(0,0);
   var W = g.getWidth();
   var H = g.getHeight();
   if (title) {
-    g.drawString(title,W/2,24);
-    var w = (g.stringWidth(title)+16)/2;
-    g.fillRect((W/2)-w,34,(W/2)+w,35);
+    g.drawString(title,W/2,6);
+    var w = (g.stringWidth(title)+8)/2;
+    g.fillRect((W/2)-w,10,(W/2)+w,10);
   }
   var lines = msg.split("\n");
-  var offset = (H - lines.length*16)/2;
+  var offset = (H + 10 - lines.length*7)/2;
   lines.forEach((line,y)=>
-    g.drawString(line,W/2,offset + y*16));
+    g.drawString(line,W/2,offset + y*7));
   g.flip();
 })
