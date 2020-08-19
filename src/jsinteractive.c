ac33ff7ab453484c06a169bf10f636f8df975dbd
Gordon Williams <gw@pur3.co.uk>
2020-08-19 11:34:31 +0100

    fix compile warning

--- src/jsinteractive.c
+++ src/jsinteractive.c
@@ -2037,7 +2037,6 @@ void jsiIdle() {
   // Go through all intervals and decrement time
   jsvObjectIteratorNew(&it, timerArrayPtr);
   while (jsvObjectIteratorHasValue(&it) && !(jsiStatus & JSIS_TIMERS_CHANGED)) {
-    bool hasDeletedTimer = false;
     JsVar *timerPtr = jsvObjectIteratorGetValue(&it);
     JsSysTime timerTime = (JsSysTime)jsvGetLongIntegerAndUnLock(jsvObjectGetChild(timerPtr, "time", 0));
     JsSysTime timeUntilNext = timerTime - timePassed;
