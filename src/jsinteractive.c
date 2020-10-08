93510f726ab493b5c4aa1a7db4e6a24fe2536254
Gordon Williams <gw@pur3.co.uk>
2020-10-08 10:43:55 +0100

    nRF52: CPU now sleeps when while UART/BLE data is waiting to be sent (fix #1938)

--- src/jsinteractive.c
+++ src/jsinteractive.c
@@ -2278,9 +2278,8 @@ void jsiIdle() {
 #if defined(USB) && !defined(EMSCRIPTEN)
       !jshIsUSBSERIALConnected() && // if USB is on, no point sleeping (later, sleep might be more drastic)
 #endif
-      !jshHasEvents() && //no events have arrived in the mean time
-      !jshHasTransmitData()/* && //nothing left to send over serial?
-      minTimeUntilNext > SYSTICK_RANGE*5/4*/) { // we are sure we won't miss anything - leave a little leeway (SysTick will wake us up!)
+      !jshHasEvents() //no events have arrived in the mean time
+      ) {
     jshSleep(minTimeUntilNext);
   }
 }
