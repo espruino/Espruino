173721f9f3fd08bf9370ef04310327b227506805
Gordon Williams <gw@pur3.co.uk>
2021-04-07 15:08:40 +0100

    Another tweak for #1995

--- targets/nrf5x/jshardware.c
+++ targets/nrf5x/jshardware.c
@@ -1073,8 +1073,8 @@ void jshPinSetState(Pin pin, JshPinState state) {
   // actually set pin config
   switch (state) {
     case JSHPINSTATE_UNDEFINED :
-    case JSHPINSTATE_ADC_IN :
-      reg->PIN_CNF[ipin] = sense
+    case JSHPINSTATE_ADC_IN : // disable sense for undefined/ADC
+      reg->PIN_CNF[ipin] = (GPIO_PIN_CNF_SENSE_Disabled << GPIO_PIN_CNF_SENSE_Pos)
                               | (GPIO_PIN_CNF_DRIVE_S0S1 << GPIO_PIN_CNF_DRIVE_Pos)
                               | (GPIO_PIN_CNF_PULL_Disabled << GPIO_PIN_CNF_PULL_Pos)
                               | (GPIO_PIN_CNF_INPUT_Disconnect << GPIO_PIN_CNF_INPUT_Pos)
