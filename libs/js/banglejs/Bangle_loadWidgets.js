c886fa247b0fb43f7a939fbf32e8d416f42a1268
Gordon Williams <gw@pur3.co.uk>
2021-05-26 16:36:26 +0100

    wrap widget draw/load in a try..catch

--- libs/js/banglejs/Bangle_loadWidgets.js
+++ libs/js/banglejs/Bangle_loadWidgets.js
@@ -1,4 +1,4 @@
 (function() {
   global.WIDGETS={};
-  require("Storage").list(/\.wid\.js$/).forEach(widget=>eval(require("Storage").read(widget)));
+  try { require("Storage").list(/\.wid\.js$/).forEach(widget=>eval(require("Storage").read(widget))); } catch (e) {print(e);}
 })
