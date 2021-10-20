eb42e9e059de03686cda1669cff29415bf47e879
Gordon Williams <gw@pur3.co.uk>
2021-10-20 16:10:02 +0100

    add emulator files that were accidentally skipped

---
+++ libs/misc/jswrap_emulated.c
@@ -0,0 +1,53 @@
+/*
+ * This file is part of Espruino, a JavaScript interpreter for Microcontrollers
+ *
+ * Copyright (C) 2013 Gordon Williams <gw@pur3.co.uk>
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ * ----------------------------------------------------------------------------
+ * This file is designed to be parsed during the build process
+ *
+ * Stubs for emulation (eg NRF class)
+ * ----------------------------------------------------------------------------
+ */
+#include <jswrap_emulated.h>
+
+/* DO_NOT_INCLUDE_IN_DOCS - this is a special token for common.py */
+
+/*JSON{
+  "type" : "class",
+  "class" : "NRF"
+}
+*/
+/*JSON{
+  "type" : "class",
+  "class" : "Bluetooth"
+}
+*/
+
+/*JSON{
+    "type" : "staticmethod",
+    "class" : "NRF",
+    "name" : "getSecurityStatus",
+    "generate_full" : "jsvNewObject()",
+    "return" : ["JsVar", "An object" ]
+}
+*/
+/*JSON{
+    "type" : "staticmethod",
+    "class" : "NRF",
+    "name" : "getAddress",
+    "generate_full" : "jsvNewFromString(\"12:34:56:78:90:ab\")",
+    "return" : ["JsVar", "An object" ]
+}
+*/
+/*JSON{
+    "type" : "staticmethod",
+    "class" : "Bluetooth",
+    "name" : "setConsole",
+    "generate_full" : ""
+}
+*/
