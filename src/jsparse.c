e2983120a5319d8b38ab92bd4f5fe14f604c9817
Gordon Williams <gw@pur3.co.uk>
2022-01-12 14:29:11 +0000

    Revert "Revert "fix potential buffer overflow (fix #2121)""
    This reverts commit 6c469f75d552c2fc35568b480af2edc132698b6c.

--- src/jsparse.c
+++ src/jsparse.c
@@ -1438,7 +1438,8 @@ NO_INLINE JsVar *jspeAddNamedFunctionParameter(JsVar *funcVar, JsVar *name) {
   if (!funcVar) funcVar = jsvNewWithFlags(JSV_FUNCTION);
   char buf[JSLEX_MAX_TOKEN_LENGTH+1];
   buf[0] = '\xFF';
-  jsvGetString(name, &buf[1], JSLEX_MAX_TOKEN_LENGTH);
+  size_t l = jsvGetString(name, &buf[1], JSLEX_MAX_TOKEN_LENGTH);
+  buf[l] = 0; // zero terminate since jsvGetString doesn't add one
   JsVar *param = jsvAddNamedChild(funcVar, 0, buf);
   jsvMakeFunctionParameter(param);
   jsvUnLock(param);
