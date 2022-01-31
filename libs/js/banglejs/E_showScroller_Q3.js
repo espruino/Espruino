d1011a1985d27a8bc765f05670972156c55958a2
Gordon Williams <gw@pur3.co.uk>
2022-01-31 09:47:29 +0000

    Bangle.js2: E.showScroller to respect widgets on bottom row (using appRect)

--- libs/js/banglejs/E_showScroller_Q3.js
+++ libs/js/banglejs/E_showScroller_Q3.js
@@ -10,27 +10,26 @@ if (!options) return;
 var menuScroll = 0;
 var rScroll = 0; // rendered menu scroll (we only shift by 2 because of dither)
 var menuShowing = false;
-var w = g.getWidth();
-var h = g.getHeight();
+var R = Bangle.appRect;
 var Y = Bangle.appRect.y;
-var n = Math.ceil((h-Y)/options.h);
-var menuScrollMax = options.h*options.c - (h-Y);
+var n = Math.ceil(R.h/options.h);
+var menuScrollMax = options.h*options.c - R.h;
 
 function idxToY(i) {
-  return i*options.h + Y - rScroll;
+  return i*options.h + R.y - rScroll;
 }
 function YtoIdx(y) {
-  return Math.floor((y + rScroll - Y)/options.h);
+  return Math.floor((y + rScroll - R.y)/options.h);
 }
 
 function drawMenu() {
-  g.reset().clearRect(0,Y,w-1,h-1);
-  g.setClipRect(0,Y,w-1,h-1);
-  var a = YtoIdx(Y);
-  var b = Math.min(YtoIdx(h-1),options.c-1);
+  g.reset().clearRect(R.x,R.y,R.x2,R.y2);
+  g.setClipRect(R.x,R.y,R.x2,R.y2);
+  var a = YtoIdx(R.y);
+  var b = Math.min(YtoIdx(R.y2),options.c-1);
   for (var i=a;i<=b;i++)
-    options.draw(i, {x:0,y:idxToY(i),w:w,h:options.h});
-  g.setClipRect(0,0,w-1,h-1);
+    options.draw(i, {x:R.x,y:idxToY(i),w:R.w,h:options.h});
+  g.setClipRect(0,0,g.getWidth()-1,g.getHeight()-1);
 }
 drawMenu();
 g.flip(); // force an update now to make this snappier
@@ -46,33 +45,33 @@ Bangle.dragHandler = e=>{
   rScroll = menuScroll &~1;
   dy = oldScroll-rScroll;
   if (!dy) return;
-  g.reset().setClipRect(0,Y,g.getWidth()-1,g.getHeight()-1);
+  g.reset().setClipRect(R.x,R.y,R.x2,R.y2);
   g.scroll(0,dy);
   var d = e.dy;
   if (d < 0) {
-    g.setClipRect(0,h-(1-d),w-1,h-1);
-    let i = YtoIdx(h-(1-d));
+    g.setClipRect(R.x,R.y2-(1-d),R.x2,R.y2);
+    let i = YtoIdx(R.y2-(1-d));
     let y = idxToY(i);
-    while (y < h) {
-      options.draw(i, {x:0,y:y,w:w,h:options.h});
+    while (y < R.y2) {
+      options.draw(i, {x:R.x,y:y,w:R.w,h:options.h});
       i++;
       y += options.h;
     }
   } else { // d>0
-    g.setClipRect(0,Y,w-1,Y+d);
-    let i = YtoIdx(Y+d);
+    g.setClipRect(R.x,R.y,R.x2,R.y+d);
+    let i = YtoIdx(R.y+d);
     let y = idxToY(i);
-    while (y > Y-options.h) {
-      options.draw(i, {x:0,y:y,w:w,h:options.h});
+    while (y > R.y-options.h) {
+      options.draw(i, {x:R.x,y:y,w:R.w,h:options.h});
       y -= options.h;
       i--;
     }
   }
-  g.setClipRect(0,0,w-1,h-1);
+  g.setClipRect(0,0,g.getWidth()-1,g.getHeight()-1);
 };
 Bangle.on('drag',Bangle.dragHandler);
 Bangle.touchHandler = (_,e)=>{
-  if (e.y<Y-4) return;
+  if (e.y<R.y-4) return;
   var i = YtoIdx(e.y);
   if (i>=0 && i<options.c)
     options.select(i);
