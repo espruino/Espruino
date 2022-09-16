11ee5457c19798fa459b0c6e1fceeae124ba9e50
Gordon Williams <gw@pur3.co.uk>
2022-09-16 14:18:32 +0100

    Fix print(()=>{}) printing 'function (undefined) {}' - saves 1 var for arrow fn without args (fix #2265)

--- src/jsparse.c
+++ src/jsparse.c
@@ -1517,13 +1517,15 @@ JsVar *jspeTemplateLiteral() {
 
 NO_INLINE JsVar *jspeAddNamedFunctionParameter(JsVar *funcVar, JsVar *name) {
   if (!funcVar) funcVar = jsvNewWithFlags(JSV_FUNCTION);
-  char buf[JSLEX_MAX_TOKEN_LENGTH+1];
-  buf[0] = '\xFF';
-  size_t l = jsvGetString(name, &buf[1], JSLEX_MAX_TOKEN_LENGTH);
-  buf[l+1] = 0; // zero terminate since jsvGetString doesn't add one
-  JsVar *param = jsvAddNamedChild(funcVar, 0, buf);
-  jsvMakeFunctionParameter(param);
-  jsvUnLock(param);
+  if (name) {
+    char buf[JSLEX_MAX_TOKEN_LENGTH+1];
+    buf[0] = '\xFF';
+    size_t l = jsvGetString(name, &buf[1], JSLEX_MAX_TOKEN_LENGTH);
+    buf[l+1] = 0; // zero terminate since jsvGetString doesn't add one
+    JsVar *param = jsvAddNamedChild(funcVar, 0, buf);
+    jsvMakeFunctionParameter(param);
+    jsvUnLock(param);
+  }
   return funcVar;
 }
 
