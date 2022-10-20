0b2308338a122b062d6be61be24393f9ef373b7b
Martin Boonk <martin@boonk.info>
2022-10-20 20:21:35 +0200

    Use configured launcher if available

--- libs/js/banglejs/Bangle_showLauncher.js
+++ libs/js/banglejs/Bangle_showLauncher.js
@@ -1,12 +1,24 @@
 (function() {
-  var l = require("Storage").list(/\.info$/).map(file => {
-    var app = require("Storage").readJSON(file,1);
-    if (app && app.type == "launch") return app;
-  }).find(x=>x);
-  if (l) {
+  var launcherApp=(require("Storage").readJSON("setting.json",1)||{}).launcher;
+  if (require("Storage").read(launcherApp)==undefined) {
+    launcherApp = require("Storage").list(/\.info$/)
+      .map(file => {
+        const app = require("Storage").readJSON(file,1);
+        if (app && app.type == "launch") {
+          return app;
+        }
+      })
+      .filter(x=>x)
+      .sort((a, b) => a.sortorder - b.sortorder)[0].src;
+  }
+
+  if (require("Storage").read(launcherApp)==undefined) {
+    eval(`E.showMessage("No Launcher Found");setWatch(()=>{load();}, global.BTN2||BTN, {repeat:false,edge:"falling"});`);
+  } else {
     if (Bangle.uiRemove) {
       Bangle.setUI(); // remove all existing UI (and call Bangle.uiRemove)
-      setTimeout(eval,0,require("Storage").read(l.src)); // Load launcher direct without a reboot
-    } else load(l.src);
-  } else E.showMessage("Launcher\nnot found");
+      setTimeout(eval,0,require("Storage").read(launcherApp)); // Load launcher direct without a reboot
+    } else load(launcherApp);
+  }
+  delete launcherApp;
 })
