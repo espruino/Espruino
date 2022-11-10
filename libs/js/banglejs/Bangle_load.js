6969b2818c7a4432eeecb6e4d52bd2c30a746caf
Martin Boonk <martin@boonk.info>
2022-11-10 23:10:51 +0100

    Load default app on Bangle.load()

--- libs/js/banglejs/Bangle_load.js
+++ libs/js/banglejs/Bangle_load.js
@@ -1,4 +1,9 @@
 (function(name) {
+  if (!name) {    
+    let s = require("Storage").readJSON("setting.json",1)||{};
+    if (require("Storage").read(s.defaultApp))
+      name = s.defaultApp;
+  }
   if (Bangle.uiRemove) {
     Bangle.setUI(); // remove all existing UI (and call Bangle.uiRemove)
     __FILE__=name;
