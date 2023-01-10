99d56edc73e0299a1773199e09e4ee579dc0a919
Dimo Bozduganov <d.bozduganov@allterco.com>
2023-01-10 15:46:19 +0200

    Add functions to espruino_embedded_utils.h

--- espruino_embedded_utils.h
+++ espruino_embedded_utils.h
@@ -18,6 +18,9 @@ typedef enum {
   JSET_REFERENCEERROR
 } JsExceptionType;
 
+typedef int32_t JsVarInt;
+typedef double JsVarFloat;
+
 size_t jsvGetString(const JsVar *v, char *str, size_t len);
 JsVar *jsvAsString(JsVar *v);
 size_t jsvGetStringLength(const JsVar *v);
@@ -25,7 +28,23 @@ void jsExceptionHere(JsExceptionType type, const char *fmt, ...);
 JsVar *jsvObjectGetChild(JsVar *obj, const char *name, unsigned short createChild);
 JsVar *jsvLockAgainSafe(JsVar *var);
 void jsvUnLock(JsVar *var);
+bool jsvIsMemoryFull();
+
+bool jsvIsBoolean(const JsVar *v);
+bool jsvIsString(const JsVar *v);
 bool jsvIsFunction(const JsVar *v);
+bool jsvIsNumeric(const JsVar *v);
+bool jsvIsObject(const JsVar *v);
+bool jsvIsArray(const JsVar *v);
+bool jsvIsNull(const JsVar *v);
+
+JsVar *jsvNewFromString(const char *str);
+JsVar *jsvNewFromInteger(JsVarInt value);
+JsVar *jsvNewFromBool(bool value);
+JsVar *jsvNewFromFloat(JsVarFloat value);
+JsVar *jsvNewFromLongInteger(long long value);
+JsVar *jsvNewEmptyArray();
+JsVar *jsvNewArray(JsVar **elements, int elementCount);
 
 #ifdef __cplusplus
 }
