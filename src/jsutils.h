cfbc4040dac6a7881e3465f60adc4dd8a1e45b85
Gordon Williams <gw@pur3.co.uk>
2023-09-08 12:08:10 +0100

    2v19 release

--- src/jsutils.h
+++ src/jsutils.h
@@ -27,9 +27,9 @@
 #endif
 
 #ifndef BUILDNUMBER
-#define JS_VERSION "2v18"
+#define JS_VERSION "2v19"
 #else
-#define JS_VERSION "2v18." BUILDNUMBER
+#define JS_VERSION "2v19." BUILDNUMBER
 #endif
 /*
   In code:
