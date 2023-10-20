cc40921344a302b452e91d18fb91b8ecdc73857e
Gordon Williams <gw@pur3.co.uk>
2023-10-20 17:07:57 +0100

    update minimal py file for LRCK - and docs

--- boards/PUCKJS_MINIMAL.py
+++ boards/PUCKJS_MINIMAL.py
@@ -41,7 +41,7 @@ info = {
      'DEFINES+=-DHAL_NFC_ENGINEERING_BC_FTPAN_WORKAROUND=1', # Looks like proper production nRF52s had this issue
      # 'DEFINES+=-DCONFIG_GPIO_AS_PINRESET', # reset isn't being used, so let's just have an extra IO (needed for Puck.js V2)
      'DEFINES+=-DESPR_DCDC_ENABLE', # Ensure DCDC converter is enabled
-     'DEFINES += -DNEOPIXEL_SCK_PIN=22', # SCK pin needs defining as something unused for neopixel (HW errata means they can't be disabled)
+     'DEFINES += -DNEOPIXEL_SCK_PIN=22 -DNEOPIXEL_LRCK_PIN=16', # SCK pin needs defining as something unused for neopixel (HW errata means they can't be disabled) see https://github.com/espruino/Espruino/issues/2071
      'DEFINES += -DNRF_BLE_GATT_MAX_MTU_SIZE=53 -DNRF_BLE_MAX_MTU_SIZE=53', # increase MTU from default of 23
      'DEFINES += -DCENTRAL_LINK_COUNT=2 -DNRF_SDH_BLE_CENTRAL_LINK_COUNT=2', # allow two outgoing connections at once     
      'LDFLAGS += -Xlinker --defsym=LD_APP_RAM_BASE=0x3290', # set RAM base to match MTU=53 + CENTRAL_LINK_COUNT=2
@@ -114,19 +114,21 @@ devices = {
 };
 
 # left-right, or top-bottom order
-board = {
+board_v1 = {
   'bottom' : [ 'D28', 'D29', 'D30', 'D31'],
   'right' : [ 'GND', '3V', 'D2', 'D1' ],
   'left2' : [ 'D6','D7','D8','D11','D13','D14','D16','D23','D24','D27' ],
   'right2' : [ 'D15' ],
+  '_title' : "Puck.js 1",    
+  '_class' : "board_v1",    
+  '_hide_not_on_connectors' : True,    
   '_notes' : {
     'D11' : "Capacitive sense. D12 is connected to this pin via a 1 MOhm resistor",
     'D28' : "If pulled up to 1 on startup, D28 and D29 become Serial1",
-    'D22' : "This is used as SCK when driving Neopixels with 'require('neopixel').write'"
-  }
-};
-
-board["_css"] = """
+    'D22' : "This is used as SCK when driving Neopixels, and will output a signal when 'require('neopixel').write' is called",
+    'D16' : "This is used as LRCK when driving Neopixels, and will output a signal when 'require('neopixel').write' is called"
+  },
+  "_css" : """
 #board {
   width: 800px;
   height: 800px;
@@ -137,29 +139,112 @@ board["_css"] = """
 #boardcontainer {
   height: 900px;
 }
-#bottom {
+.board_v1 #bottom {
     top: 639px;
     left: 291px;
 }
-#right {
+.board_v1 #right {
     top: 304px;
     left: 640px;
 }
 
-.bottompin { width: 46px; }
-.rightpin { height: 51px; }
-.pinD6 { position:absolute; left: 560px; top: 419px;}
-.pinD7 { position:absolute; left: 548px; top: 369px;}
-.pinD8 { position:absolute; left: 512px; top: 398px;}
-.pinD11 { position:absolute; left: 586px; top: 236px;}
-.pinD13 { position:absolute; left: 500px; top: 293px;}
-.pinD14 { position:absolute; left: 523px; top: 270px;}
-.pinD15 { position:absolute; right: -483px; top: 268px;}
-.pinD16 { position:absolute; left: 499px; top: 244px;}
-.pinD23 { position:absolute; left: 157px; top: 438px;}
-.pinD24 { position:absolute; left: 157px; top: 382px;}
-.pinD27 { position:absolute; left: 244px; top: 581px;}
-""";
+.board_v1 .bottompin { width: 46px; }
+.board_v1 .rightpin { height: 51px; }
+.board_v1 .pinD6 { position:absolute; left: 560px; top: 419px;}
+.board_v1 .pinD7 { position:absolute; left: 548px; top: 369px;}
+.board_v1 .pinD8 { position:absolute; left: 512px; top: 398px;}
+.board_v1 .pinD11 { position:absolute; left: 586px; top: 236px;}
+.board_v1 .pinD13 { position:absolute; left: 500px; top: 293px;}
+.board_v1 .pinD14 { position:absolute; left: 523px; top: 270px;}
+.board_v1 .pinD15 { position:absolute; right: -483px; top: 268px;}
+.board_v1 .pinD16 { position:absolute; left: 499px; top: 244px;}
+.board_v1 .pinD23 { position:absolute; left: 157px; top: 438px;}
+.board_v1 .pinD24 { position:absolute; left: 157px; top: 382px;}
+.board_v1 .pinD27 { position:absolute; left: 244px; top: 581px;}
+.board_v1 .pinD6 .DEVICE { display:none; }
+.board_v1 .pinD7 .DEVICE { display:none; }
+.board_v1 .pinD8 .DEVICE { display:none; }
+/* allow for D11 capsense */
+.board_v1 .pinD13 .DEVICE { display:none; }
+.board_v1 .pinD14 .DEVICE { display:none; }
+.board_v1 .pinD15 .DEVICE { display:none; }
+"""};
+board_v2 = {
+  'bottom' : [ 'D28', 'D29', 'D30', 'D31'],
+  'right' : [ 'GND', '3V', 'D2', 'D1' ],
+  'left2' : [ 'D11','D23','D24','D26' ],
+  '_title' : "Puck.js 2",      
+  '_class' : "board_v2",  
+  '_notes' : {
+    'D11' : "Capacitive sense. D12 is connected to this pin via a 1 MOhm resistor",
+    'D26' : "This is connected via a FET. If D26=1, this pin is shorted to GND, if D26=0 the pin is floating",
+    'D28' : "If pulled up to 1 on startup, D28 and D29 become Serial1",
+    'D22' : "This is used as SCK when driving Neopixels, and will output a signal when 'require('neopixel').write' is called",
+    'D16' : "This is used as LRCK when driving Neopixels, and will output a signal when 'require('neopixel').write' is called"
+  },
+  "_css" : """
+#board {
+  width: 800px;
+  height: 800px;
+  top: 0px;
+  left : 0px;
+  background-image: url(img/PUCKJS_2.jpg);
+}
+#boardcontainer {
+  height: 900px;
+}
+.board_v2 #bottom {
+    top: 655px;
+    left: 294px;
+}
+.board_v2 #right {
+    top: 310px;
+    left: 640px;
+}
+
+.board_v2 .bottompin { width: 46px; }
+.board_v2 .rightpin { height: 51px; }
+.board_v2 .pinD11 { position:absolute; left: 609px; top: 264px;}
+.board_v2 .pinD23 { position:absolute; left: 126px; top: 414px;}
+.board_v2 .pinD24 { position:absolute; left: 126px; top: 382px;}
+.board_v2 .pinD26 { position:absolute; left: 258px; top: 593px;}
+.board_v2 .pinD26 .DEVICE { display:none; }
+"""};
+board_lite = {
+  'bottom' : [ 'D28', 'D29', 'D30', 'D31'],
+  'right' : [ 'GND', '3V', 'D2', 'D1' ],
+  '_title' : "Puck.js Lite",    
+  '_class' : "board_lite",    
+  '_hide_not_on_connectors' : True,  
+  '_notes' : {
+    'D21' : "Marked RST. This is usable IO by default, but is also the nRF52 reset pin.",  
+    'D28' : "If pulled up to 1 on startup, D28 and D29 become Serial1",
+  },
+  "_css" : """
+#board {
+  width: 800px;
+  height: 800px;
+  top: 0px;
+  left : 0px;
+  background-image: url(img/PUCKJS_LITE.jpg);
+}
+#boardcontainer {
+  height: 900px;
+}
+.board_lite #bottom {
+    top: 649px;
+    left: 291px;
+}
+.board_lite #right {
+    top: 298px;
+    left: 640px;
+}
+
+.board_lite .bottompin { width: 46px; }
+.board_lite .rightpin { height: 51px; }
+"""};
+
+boards = [board_v2, board_v1, board_lite];
 
 def get_pins():
   pins = pinutils.generate_pins(0,31) # 32 General Purpose I/O Pins.
@@ -179,7 +264,8 @@ def get_pins():
   pinutils.findpin(pins, "PD31", True)["functions"]["ADC1_IN7"]=0;
   # everything is non-5v tolerant
   for pin in pins:
-    pin["functions"]["3.3"]=0;
+    if pin["name"]!="PD26":
+      pin["functions"]["3.3"]=0;
 
   #The boot/reset button will function as a reset button in normal operation. Pin reset on PD21 needs to be enabled on the nRF52832 device for this to work.
   return pins
