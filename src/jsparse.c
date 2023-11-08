espruino/Espruino

commit f87a53c60f71b8107262266a544c1ff210c76ec7

Ensure AssignmentExpr returns the value of LHS, not the LHS. `(a=2)=3` now fails (as per spec)             Ensure default args for arrow functions fail with error `(a,b=3)=>a+b` now fails (until default args get added)

src/jsparse.c
@@ -2205,6 +2205,7 @@ NO_INLINE JsVar *__jspeAssignmentExpression(JsVar *lhs) {
       }
     }
     jsvUnLock(rhs);
+    lhs = jsvSkipNameAndUnLock(lhs);
   }
   return lhs;
 }
@@ -2318,6 +2319,7 @@ NO_INLINE JsVar *jspeBlockOrStatement() {
     return 0;
   } else {
     JsVar *v = jspeStatement();
+    if (lex->tk==';') JSP_ASSERT_MATCH(';');
     return v;
   }
 }
@@ -2329,7 +2331,6 @@ NO_INLINE JsVar *jspParse() {
   while (!JSP_SHOULDNT_PARSE && lex->tk != LEX_EOF) {
     jsvUnLock(v);
     v = jspeBlockOrStatement();
-    while (lex->tk==';') JSP_ASSERT_MATCH(';');
     jsvCheckReferenceError(v);
   }
   return v;
@@ -2410,9 +2411,7 @@ NO_INLINE JsVar *jspeStatementIf() {
   JSP_SAVE_EXECUTE();
   if (!cond) jspSetNoExecute();
   JsExecFlags hasError = 0;
-  JsVar *a = 0;
-  if (lex->tk!=';')
-    a = jspeBlockOrStatement();
+  JsVar *a = jspeBlockOrStatement();
   hasError |= execInfo.execute&EXEC_ERROR_MASK;
   if (!cond) {
     jsvUnLock(a);
@@ -2421,16 +2420,11 @@ NO_INLINE JsVar *jspeStatementIf() {
   } else {
     result = a;
   }
-  /* We must manually parse ';' here, because if we did it when execInfo.execute==false (eg `if(0);`)
-  then if a String comes straight after it wouldn't have been interpreted */
-  if (lex->tk==';') JSP_ASSERT_MATCH(';');
   if (lex->tk==LEX_R_ELSE) {
     JSP_ASSERT_MATCH(LEX_R_ELSE);
     JSP_SAVE_EXECUTE();
     if (cond) jspSetNoExecute();
-    JsVar *a = 0;
-    if (lex->tk!=';')
-      a = jspeBlockOrStatement();
+    JsVar *a = jspeBlockOrStatement();
     hasError |= execInfo.execute&EXEC_ERROR_MASK;
     if (cond) {
       jsvUnLock(a);

commit 4c13af9b373acebf488cfa8dfcc261e5ba6b9696

Reinstate `if (0);"test"` fix after f87a53c accidentally reverted it

src/jsparse.c
@@ -2324,7 +2324,6 @@ NO_INLINE JsVar *jspeBlockOrStatement() {
     return 0;
   } else {
     JsVar *v = jspeStatement();
-    if (lex->tk==';') JSP_ASSERT_MATCH(';');
     return v;
   }
 }
@@ -2336,6 +2335,7 @@ NO_INLINE JsVar *jspParse() {
   while (!JSP_SHOULDNT_PARSE && lex->tk != LEX_EOF) {
     jsvUnLock(v);
     v = jspeBlockOrStatement();
+    while (lex->tk==';') JSP_ASSERT_MATCH(';');
     jsvCheckReferenceError(v);
   }
   return v;
@@ -2416,7 +2416,9 @@ NO_INLINE JsVar *jspeStatementIf() {
   JSP_SAVE_EXECUTE();
   if (!cond) jspSetNoExecute();
   JsExecFlags hasError = 0;
-  JsVar *a = jspeBlockOrStatement();
+  JsVar *a = 0;
+  if (lex->tk!=';')
+    a = jspeBlockOrStatement();
   hasError |= execInfo.execute&EXEC_ERROR_MASK;
   if (!cond) {
     jsvUnLock(a);
@@ -2425,11 +2427,16 @@ NO_INLINE JsVar *jspeStatementIf() {
   } else {
     result = a;
   }
+  /* We must manually parse ';' here, because if we did it when execInfo.execute==false (eg `if(0);`)
+  then if a String comes straight after it wouldn't have been interpreted */
+  if (lex->tk==';') JSP_ASSERT_MATCH(';');
   if (lex->tk==LEX_R_ELSE) {
     JSP_ASSERT_MATCH(LEX_R_ELSE);
     JSP_SAVE_EXECUTE();
     if (cond) jspSetNoExecute();
-    JsVar *a = jspeBlockOrStatement();
+    JsVar *a = 0;
+    if (lex->tk!=';')
+      a = jspeBlockOrStatement();
     hasError |= execInfo.execute&EXEC_ERROR_MASK;
     if (cond) {
       jsvUnLock(a);

tests/test_string_noparse.js
@@ -16,5 +16,6 @@ if (1) 1;"test"||fail("if(1)1;")   // ok
 if (0) 1;"test"||fail("if(0)1;")   // failed
 if (0);"test"||fail("if(0);")      // failed
 if (1);else;"test"||fail("if(1);else;") // failed
+(function(){ if(0)return 42;'OK'!='OK'&&fail('if(0)return')})()
 // if we got here we're ok
 result=1