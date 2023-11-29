212f2cf1f9b1296a87f2d1c9d7f76ee5e6a8c180
Gordon Williams <gw@pur3.co.uk>
2023-11-29 13:46:43 +0000

    add test comments

--- tests/test_exception_2.js
+++ tests/test_exception_2.js
@@ -1,8 +1,10 @@
 try {
   throw "Oh Noes!";
 } finally {
+  console.log("Finally block executed");
   result = 1;
 }
+console.log("exception uncaught so nothing from here is executed");
 result = 0;
 try {
   result = 0;
