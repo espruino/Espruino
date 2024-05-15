b3c6f555abd48a447ded08226e943ce476cf9cb1
Gordon Williams <gw@pur3.co.uk>
2024-05-15 15:31:21 +0100

    update test after asImage palette fix

--- tests/test_graphics_asImage.js
+++ tests/test_graphics_asImage.js
@@ -33,7 +33,7 @@ g.palette = new Uint16Array([0,0x1234]);
 let image = g.asImage("object"); // same as no arguments
 results.push(E.toJS(image)=='{width:8,height:8,transparent:1,palette:[0,4660],buffer:"\\x80@ \\x10\\b\\4\\2\\1"}');
 let image = g.asImage("string");
-results.push(E.toUint8Array(image).slice().join(",")=="8,8,193,1,0,0,18,52,128,64,32,16,8,4,2,1");
+results.push(E.toUint8Array(image).slice().join(",")=="8,8,193,1,0,0,52,18,128,64,32,16,8,4,2,1");
 
 print("Test passes: ",results);
 
