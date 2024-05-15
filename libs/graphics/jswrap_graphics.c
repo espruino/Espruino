espruino/Espruino

commit 3e56d1e12bbfaada7edaa3171641ab2ea470011b

Fix issue using String.replace on flash-based (read only) strings

libs/graphics/jswrap_graphics.c
@@ -2495,8 +2495,7 @@ JsVar *jswrap_graphics_wrapString(JsVar *parent, JsVar *str, int maxWidth) {
           jsvUnLock(currentLine);
         }
         // Add the remaining bit of word
-        currentLine = jsvNewFromEmptyString(); // don't use jsvNewFromStringVar as it'll clone flash strings/etc
-        jsvAppendStringVar(currentLine, str, (size_t)wordStartIdx, (size_t)(currentPos-(wordStartIdx+1)));
+        currentLine = jsvNewWritableStringFromStringVar(str, (size_t)wordStartIdx, (size_t)(currentPos-(wordStartIdx+1)));
 #ifdef ESPR_UNICODE_SUPPORT
         if (jsvIsUTF8String(str))
           currentLine = jsvNewUTF8StringAndUnLock(currentLine);

libs/network/jswrap_net.c
@@ -142,11 +142,11 @@ JsVar *jswrap_url_parse(JsVar *url, bool parseQuery) {
 
   JsVar *v;
 
-  v = jsvNewFromStringVar(url, (size_t)pathStart, JSVAPPENDSTRINGVAR_MAXLENGTH);
+  v = jsvNewWritableStringFromStringVar(url, (size_t)pathStart, JSVAPPENDSTRINGVAR_MAXLENGTH);
   if (jsvGetStringLength(v)==0) jsvAppendString(v, "/");
   jsvObjectSetChildAndUnLock(obj, "path", v);
 
-  v = jsvNewFromStringVar(url, (size_t)pathStart, (size_t)((searchStart>=0)?(searchStart-pathStart):JSVAPPENDSTRINGVAR_MAXLENGTH));
+  v = jsvNewWritableStringFromStringVar(url, (size_t)pathStart, (size_t)((searchStart>=0)?(searchStart-pathStart):JSVAPPENDSTRINGVAR_MAXLENGTH));
   if (jsvGetStringLength(v)==0) jsvAppendString(v, "/");
   jsvObjectSetChildAndUnLock(obj, "pathname", v);
 

src/jsvar.c
@@ -1924,7 +1924,20 @@ void jsvAppendStringVar(JsVar *var, const JsVar *str, size_t stridx, size_t maxL
   jsvStringIteratorFree(&dst);
 }
 
-/** Create a new variable from a substring. argument must be a string. stridx = start char or str, maxLength = max number of characters (can be JSVAPPENDSTRINGVAR_MAXLENGTH) */
+/** Create a new String from a substring in RAM. It is always writable. jsvNewFromStringVar can reference a non-writable string.
+The Argument must be a string. stridx = start char or str, maxLength = max number of characters (can be JSVAPPENDSTRINGVAR_MAXLENGTH) */
+JsVar *jsvNewWritableStringFromStringVar(const JsVar *str, size_t stridx, size_t maxLength) {
+  JsVar *var = jsvNewFromEmptyString();
+  jsvAppendStringVar(var, str, stridx, maxLength);
+#ifdef ESPR_UNICODE_SUPPORT
+  if (jsvIsUTF8String(str))
+    var = jsvNewUTF8StringAndUnLock(var);
+#endif
+  return var;
+}
+
+/** Create a new variable from a substring. If a Native or Flash String, the memory area will be referenced (so the new string may not be writable)
+The Argument must be a string. stridx = start char or str, maxLength = max number of characters (can be JSVAPPENDSTRINGVAR_MAXLENGTH) */
 JsVar *jsvNewFromStringVar(const JsVar *str, size_t stridx, size_t maxLength) {
   if (jsvIsNativeString(str) || jsvIsFlashString(str)) {
     // if it's a flash string, just change the pointer (but we must check length)
@@ -1936,18 +1949,12 @@ JsVar *jsvNewFromStringVar(const JsVar *str, size_t stridx, size_t maxLength) {
     res->varData.nativeStr.len = (JsVarDataNativeStrLength)maxLength;
     return res;
   }
-  JsVar *var = jsvNewFromEmptyString();
-  jsvAppendStringVar(var, str, stridx, maxLength);
-#ifdef ESPR_UNICODE_SUPPORT
-  if (jsvIsUTF8String(str))
-    var = jsvNewUTF8StringAndUnLock(var);
-#endif
-  return var;
+  return jsvNewWritableStringFromStringVar(str, stridx, maxLength);
 }
 
-/** Create a new variable from a string. argument must be a string. */
+/** Create a new writable string from a string (flash/native strings will be cloned, not referenced). argument must be a string. */
 JsVar *jsvNewFromStringVarComplete(JsVar *var) {
-  return jsvNewFromStringVar(var, 0, JSVAPPENDSTRINGVAR_MAXLENGTH);
+  return jsvNewWritableStringFromStringVar(var, 0, JSVAPPENDSTRINGVAR_MAXLENGTH);
 }
 
 /** Append all of str to var. Both must be strings.  */
@@ -3875,8 +3882,7 @@ JsVar *jsvMathsOp(JsVar *a, JsVar *b, int op) {
         // It's a string, but it can't be appended - don't copy as copying will just keep the same var type!
         // Instead we create a new string var by copying
         // opt: should we allocate a flat string here? but repeated appends would then be slow
-        v = jsvNewFromEmptyString(); // don't use jsvNewFromStringVar as this does a copy for native/flash strs too
-        if (v) jsvAppendStringVarComplete(v, da);
+        v = jsvNewFromStringVarComplete(da);
       } else // otherwise just copy it
         v = jsvCopy(da, false);
       if (v) // could be out of memory

src/jsvar.h
@@ -315,8 +315,13 @@ JsVar *jsvNewUTF8String(JsVar* dataString); ///< Create a new unicode string usi
 JsVar *jsvNewUTF8StringAndUnLock(JsVar* dataString); ///< Create a new unicode string using the given data string for backing
 #endif
 static ALWAYS_INLINE JsVar *jsvNewNull() { return jsvNewWithFlags(JSV_NULL); } ;///< Create a new null variable
-/** Create a new variable from a substring. argument must be a string. stridx = start char or str, maxLength = max number of characters (can be JSVAPPENDSTRINGVAR_MAXLENGTH)  */
+/** Create a new String from a substring in RAM. It is always writable. jsvNewFromStringVar can reference a non-writable string.
+The Argument must be a string. stridx = start char or str, maxLength = max number of characters (can be JSVAPPENDSTRINGVAR_MAXLENGTH) */
+JsVar *jsvNewWritableStringFromStringVar(const JsVar *str, size_t stridx, size_t maxLength);
+/** Create a new variable from a substring. If a Native or Flash String, the memory area will be referenced (so the new string may not be writable)
+The Argument must be a string. stridx = start char or str, maxLength = max number of characters (can be JSVAPPENDSTRINGVAR_MAXLENGTH) */
 JsVar *jsvNewFromStringVar(const JsVar *str, size_t stridx, size_t maxLength);
+/** Create a new writable string from a string (flash/native strings will be cloned, not referenced). argument must be a string. */
 JsVar *jsvNewFromStringVarComplete(JsVar *var);
 JsVar *jsvNewFromInteger(JsVarInt value);
 JsVar *jsvNewFromBool(bool value);

src/jswrap_espruino.c
@@ -1508,7 +1508,7 @@ void jswrap_e_dumpVariables() {
     else if (jsvIsString(v)) {
       JsVar *s;
       if (jsvGetStringLength(v)>20) {
-        s = jsvNewFromStringVar(v, 0, 17);
+        s = jsvNewWritableStringFromStringVar(v, 0, 17);
         jsvAppendString(s,"...");
       } else
         s = jsvLockAgain(v);

src/jswrap_string.c
@@ -401,7 +401,7 @@ static JsVar *_jswrap_string_replace(JsVar *parent, JsVar *subStr, JsVar *newSub
 
   int idx = jswrap_string_indexOf(str, subStr, NULL, false);
   while (idx>=0  && !jspIsInterrupted()) {
-    JsVar *newStr = jsvNewFromStringVar(str, 0, (size_t)idx);
+    JsVar *newStr = jsvNewWritableStringFromStringVar(str, 0, (size_t)idx);
     jsvAppendStringVarComplete(newStr, newSubStr);
     jsvAppendStringVar(newStr, str, (size_t)idx+jsvGetStringLength(subStr), JSVAPPENDSTRINGVAR_MAXLENGTH);
     jsvUnLock(str);
@@ -783,8 +783,7 @@ original `String`.
 JsVar *jswrap_string_concat(JsVar *parent, JsVar *args) {
   if (!jsvIsString(parent)) return 0;
   // don't use jsvNewFromStringVar here because it has an optimisation for Flash Strings that just clones (rather than copying)
-  JsVar *str = jsvNewFromEmptyString();
-  jsvAppendStringVarComplete(str, parent);
+  JsVar *str = jsvNewFromStringVarComplete(parent);
   JsVar *extra = jsvArrayJoin(args, NULL/*filler*/, false/*ignoreNull*/);
   jsvAppendStringVarComplete(str, extra);
   jsvUnLock(extra);

commit e4983160e0e74bd51df5e2e3d044d08da4132cb4

fix assert fail in wrapString introduced in 3e56d1e12bbfaada7edaa3171641ab2ea470011b

libs/graphics/jswrap_graphics.c
@@ -2499,7 +2499,7 @@ JsVar *jswrap_graphics_wrapString(JsVar *parent, JsVar *str, int maxWidth) {
         // Add the remaining bit of word
         currentLine = jsvNewWritableStringFromStringVar(str, (size_t)wordStartIdx, (size_t)(currentPos-(wordStartIdx+1)));
 #ifdef ESPR_UNICODE_SUPPORT
-        if (jsvIsUTF8String(str))
+        if (jsvIsUTF8String(str) && !jsvIsUTF8String(currentLine))
           currentLine = jsvNewUTF8StringAndUnLock(currentLine);
 #endif
         if (wasNewLine) wordBreakCharacter = ' ';