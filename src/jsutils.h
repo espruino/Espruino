246a3c78722c8ed7f728a87fc34e5e4bd79d1a12
Gordon Williams <gw@pur3.co.uk>
2024-05-21 14:13:36 +0100

    2v22 release

--- src/jsutils.h
+++ src/jsutils.h
@@ -27,9 +27,9 @@
 #endif
 
 #ifndef BUILDNUMBER
-#define JS_VERSION "2v21"
+#define JS_VERSION "2v22"
 #else
-#define JS_VERSION "2v21." BUILDNUMBER
+#define JS_VERSION "2v22." BUILDNUMBER
 #endif
 /*
   In code:
