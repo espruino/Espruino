espruino/Espruino

commit 29a243226ea2ec15a68f8aed9bd3c7ff736c185c

fix accidental build issues

boards/NUCLEOF411RE.py
@@ -27,11 +27,12 @@ info = {
  'build' : {
    'optimizeflags' : '-O3',
    'libraries' : [
-     'NET',
      'GRAPHICS',
      'NEOPIXEL'
    ],
    'makefile' : [
+     'DEFINES+=-DSAVE_ON_FLASH_MATH', 
+     'DEFINES+=-DESPR_PACKED_SYMPTR', # Pack builtin symbols' offset into pointer to save 2 bytes/symbol
      'WRAPPERSOURCES+=targets/nucleo/jswrap_nucleo.c',
      'DEFINES+=-DUSE_USB_OTG_FS=1',
      'DEFINES+=-DPIN_NAMES_DIRECT=1', # Package skips out some pins, so we can't assume each port starts from 0

boards/NUCLEOF401RE.py
@@ -27,11 +27,12 @@ info = {
  'build' : {
    'optimizeflags' : '-O3',
    'libraries' : [
-     'NET',
      'GRAPHICS',
      'NEOPIXEL'
    ],
    'makefile' : [
+     'DEFINES+=-DSAVE_ON_FLASH_MATH', 
+     'DEFINES+=-DESPR_PACKED_SYMPTR', # Pack builtin symbols' offset into pointer to save 2 bytes/symbol
      'WRAPPERSOURCES+=targets/nucleo/jswrap_nucleo.c',
      'DEFINES+=-DUSE_USB_OTG_FS=1',
      'DEFINES+=-DPIN_NAMES_DIRECT=1', # Package skips out some pins, so we can't assume each port starts from 0

src/jswrapper.h
@@ -72,8 +72,6 @@ typedef enum {
 #define PACKED_JSW_SYM __attribute__((aligned(2)))
 #endif
 
-#define ESPR_PACKED_SYMPTR
-
 /// This is the Structure for storing each symbol in the list of built-in symbols (in flash)
 // JswSymPtr should be a multiple of 2 in length or jswBinarySearch will need READ_FLASH_UINT16
 #ifndef ESPR_PACKED_SYMPTR // 'Normal' symbol storage - strOffset is *not* packed into function pointer

targets/nrf5x/jshardware.c
@@ -2273,7 +2273,9 @@ void jshI2CSetup(IOEventFlags device, JshI2CInfo *inf) {
     nrf_drv_twi_config_t    p_twi_config;
     p_twi_config.scl = (uint32_t)pinInfo[inf->pinSCL].pin;
     p_twi_config.sda = (uint32_t)pinInfo[inf->pinSDA].pin;
-    p_twi_config.frequency = (nrf_drv_twi_frequency_t)((inf->bitrate<175000) ? NRF_TWI_FREQ_100K : ((inf->bitrate<325000) ? NRF_TWI_FREQ_250K : NRF_TWI_FREQ_400K));
+    // (nrf_drv_twi_frequency_t) cast can be used here on SDK15
+    p_twi_config.frequency =
+                      ((inf->bitrate<175000) ? NRF_TWI_FREQ_100K : ((inf->bitrate<325000) ? NRF_TWI_FREQ_250K : NRF_TWI_FREQ_400K));
     p_twi_config.interrupt_priority = APP_IRQ_PRIORITY_LOW;
     if (twi1Initialised) nrf_drv_twi_uninit(twi);
     twi1Initialised = true;

commit b35b9b390118654cc6872996bf0647dc414d6735

Fix NUCLEOF411RE binary
Looks like this commit https://github.com/espruino/Espruino/commit/29a243226ea2ec15a68f8aed9bd3c7ff736c185c broke the Nucleo F411 binary, details here: https://forum.espruino.com/conversations/396990/#comment17415645
Reverted the mentioned commit's changes to the F411 board file, and changed `'STLIB=STM32F401xE'` to `'STLIB=STM32F411xE'` since it's a 411 :) No idea if that makes any difference...
Did some basic testing, so far it seems to work fine.

boards/NUCLEOF411RE.py
@@ -27,16 +27,15 @@ info = {
  'build' : {
    'optimizeflags' : '-O3',
    'libraries' : [
+     'NET',
      'GRAPHICS',
      'NEOPIXEL'
    ],
    'makefile' : [
-     'DEFINES+=-DSAVE_ON_FLASH_MATH', 
-     'DEFINES+=-DESPR_PACKED_SYMPTR', # Pack builtin symbols' offset into pointer to save 2 bytes/symbol
      'WRAPPERSOURCES+=targets/nucleo/jswrap_nucleo.c',
      'DEFINES+=-DUSE_USB_OTG_FS=1',
      'DEFINES+=-DPIN_NAMES_DIRECT=1', # Package skips out some pins, so we can't assume each port starts from 0
-     'STLIB=STM32F401xE',
+     'STLIB=STM32F411xE',
      'PRECOMPILED_OBJS+=$(ROOT)/targetlibs/stm32f4/lib/startup_stm32f401xx.o'
    ]
   }