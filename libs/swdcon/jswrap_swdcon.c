baf64ef054b34097441b39a9f47b72dbebb01c4f
fanoush <fanoush@gmail.com>
2024-08-19 01:53:48 +0200

    use utility timer to poll RTT buffers

--- libs/swdcon/jswrap_swdcon.c
+++ libs/swdcon/jswrap_swdcon.c
@@ -13,7 +13,7 @@
  */
 #include "jsvariterator.h"
 #include "jsinteractive.h"
-
+#include "jstimer.h"
 #ifdef NRF5X
 #include "app_util_platform.h"
 #endif
@@ -38,19 +38,32 @@
 //#include "SEGGER_RTT_Conf.h"
 #include "SEGGER_RTT_custom.c"
 
+// Forward function declarations
+bool swdconActivate();
+void swdconRelease();
+bool swdconRecv();
+bool swdconSend();
+void swdconUtilTimerTask(JsSysTime time, void* userdata);
+void swdconEnablePolling(int interval);
+void swdconDisablePolling();
+
+// console data structures
+
 // possibly dynamically allocated from variables?
 static char swdconUpBuffer[BUFFER_SIZE_UP];
 static char swdconDownBuffer[BUFFER_SIZE_DOWN];
 const char swdconBufferName[] ="SWDCON"; // visible in "rtt channels" openocd command, SEGGER default for channel 0 is "Terminal"
-// Forward function declarations
-
-
-// console data structures
 
 #define MODE_OFF 0    // console is off
 #define MODE_ON  1    // console is on
 
-static uint8_t      srvMode = MODE_OFF;    ///< current mode 
+static struct {
+  uint8_t srvMode:1;
+  bool timerRunning:1;
+  bool timerSlow:1;
+} flags;
+
+uint32_t timerIdleCounter=0;
 
 /*JSON{
   "type" : "object",
@@ -70,9 +83,9 @@ uses SEGGER RTT so it can be used with openocd and other SEGGER compatible tools
 */
 void jswrap_swdcon_init(void) {
   SEGGER_RTT_Init();
-  SEGGER_RTT_ConfigDownBuffer(0, swdconBufferName, swdconDownBuffer, sizeof(swdconDownBuffer), SEGGER_RTT_MODE_DEFAULT);
+  SEGGER_RTT_ConfigDownBuffer(0, swdconBufferName, swdconDownBuffer, sizeof(swdconDownBuffer), SEGGER_RTT_MODE_BLOCK_IF_FIFO_FULL);
   SEGGER_RTT_ConfigUpBuffer(0, swdconBufferName, swdconUpBuffer, sizeof(swdconUpBuffer), SEGGER_RTT_MODE_DEFAULT);
-  srvMode = MODE_ON; // hardcoded for now
+  flags.srvMode = MODE_ON; // hardcoded for now
 }
 
 /*JSON{
@@ -81,7 +94,8 @@ void jswrap_swdcon_init(void) {
 }
 */
 void jswrap_swdcon_kill(void) {
-  srvMode = MODE_OFF;
+  flags.srvMode = MODE_OFF;
+  swdconDisablePolling();
 }
 
 /*JSON{
@@ -89,26 +103,82 @@ void jswrap_swdcon_kill(void) {
   "generate" : "jswrap_swdcon_idle"
 }
 */
-bool swdconActivate();
-bool swdconRecv();
 
 bool jswrap_swdcon_idle(void) {
 
-  if (srvMode == MODE_OFF) {
+  if (flags.srvMode == MODE_OFF) {
     return false;
   }
 
-  if (SEGGER_RTT_HasKey()) swdconActivate();
+  if (SEGGER_RTT_HasKey()){
+    swdconActivate();
+    if (!flags.timerRunning){
+      swdconEnablePolling(50);
+    }
+  } else {
+    if (flags.timerRunning){
+      if (timerIdleCounter < 10 && flags.timerSlow){
+        // restart timer in fast mode
+        swdconDisablePolling();
+        flags.timerSlow=false;
+        swdconEnablePolling(50);
+        timerIdleCounter = 0;
+
+      }
+      if (timerIdleCounter > 200 && !flags.timerSlow){
+        // slow down timer if idle for too long (>10 sec = 50*200ms)
+        swdconDisablePolling();
+        swdconEnablePolling(500);
+        flags.timerSlow = true;
+        timerIdleCounter = 10;
+      }
+      // stop polling if idle for 10min=500*1200ms
+      if (timerIdleCounter > 1200 && flags.timerSlow){
+        swdconDisablePolling();
+        flags.timerSlow=false;
+        timerIdleCounter = 0;
+      }
+    }
+  }
+
   bool active = false;
-  active |= swdconRecv();
-   // prevent sleep if SWD console is active, we probably have power attached anyway
-   // sleeping would work if SWD debugger would trigger interrupt after sending data (it does not)
-   // however it is possible to trigger interrupt in our own SWD RTT host in future
-   // e.g. triggering TIMER1 interrupt vie writing STIR register wakes nrf52 espruino device
-  active |= (jsiGetConsoleDevice() == EV_SWDCON);
+  if (!flags.timerRunning){
+    // no polling in timer, do it here instead
+    active |= swdconRecv();
+    active |= swdconSend();
+    // prevent sleep if SWD console is active, we probably have power attached anyway
+    // sleeping would work if SWD debugger would trigger interrupt after sending data (it does not)
+    // however it is possible to trigger interrupt in our own SWD RTT host in future
+    // e.g. triggering TIMER1 interrupt vie writing STIR register wakes nrf52 espruino device
+    active |= (jsiGetConsoleDevice() == EV_SWDCON);
+  }
   return active;  
 }
+void swdconUtilTimerTask(JsSysTime time, void* userdata){
+//  if (jsiGetConsoleDevice() != EV_SWDCON)
+//    return;
+  bool active = false;
+  active |= swdconRecv();
+  active |= swdconSend();
+  if (active)
+    timerIdleCounter=0;
+  else
+    timerIdleCounter++;
+}
 
+void swdconEnablePolling(int interval){
+  if (!flags.timerRunning){
+    JsSysTime t = jshGetTimeFromMilliseconds(interval);
+    flags.timerRunning = jstExecuteFn(&swdconUtilTimerTask,NULL,t,t,NULL);
+  }
+}
+
+void swdconDisablePolling(){
+  if (flags.timerRunning){
+    jstStopExecuteFn(&swdconUtilTimerTask,NULL);
+    flags.timerRunning = false;
+  }
+}
 //===== Internal functions
 bool wasForced=false;
 IOEventFlags oldConsole = EV_NONE;
@@ -135,7 +205,6 @@ bool swdconActivate() {
   }
   return true;
 }
-
 // Close the connection and release the console device
 void swdconRelease() {
   IOEventFlags console = jsiGetConsoleDevice();
@@ -150,25 +219,36 @@ void swdconRelease() {
   }
 }
 
-void swdconSendChar(char ch) {
-  if (srvMode == MODE_OFF) return;
-  int timeout = WAIT_UNTIL_N_CYCLES;
-  while (SEGGER_RTT_GetAvailWriteSpace(0)==0){
-    // same handling as in jshTransmit
-    if (jshIsInInterrupt()) {
-      // if we're printing from an IRQ, don't wait - it's unlikely TX will ever finish
-      jsErrorFlags |= JSERR_BUFFER_FULL;
-      return;
-    }
-    jshBusyIdle();
-    if (--timeout == 0) {
-      // try to switch away if buffer is full and nobody is reading
+// handle full Espruino transmit buffer
+void swdconBusyIdle(int loopCount){
+  if (flags.srvMode == MODE_OFF) return;
+  if (flags.timerRunning || SEGGER_RTT_GetAvailWriteSpace(0)==0){
+    // we can't send to host here
+    if (loopCount>10000){
+      // if it takes too long and nobody is reading then give up and try to switch away
+      jshTransmitClearDevice(EV_SWDCON);
       jsiStatus |= JSIS_ECHO_OFF_FOR_LINE; // we are full, no space for "<-" when switching
       swdconRelease();
-      return;
+      jsErrorFlags |= JSERR_BUFFER_FULL;
     }
-  } 
-  SEGGER_RTT_PutChar(0,ch);
+    return;
+  }
+  // ok, we can try to send data to RTT host now
+  int ch = jshGetCharToTransmit(EV_SWDCON);
+  if (ch < 0) return;
+  SEGGER_RTT_PutChar(0,(char)ch);
+}
+
+bool swdconSend(){
+  bool gotChar=false;
+  while (SEGGER_RTT_GetAvailWriteSpace(0)>0){
+    int ch = jshGetCharToTransmit(EV_SWDCON);
+    if (ch<0)
+      break;
+    SEGGER_RTT_PutChar(0,(char)ch);
+    gotChar=true;
+  }
+  return gotChar;
 }
 
 bool swdconRecv() {
