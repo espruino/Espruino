d3a11585b28a75338650bda552719758548c7650
Richard Blakesley <richard.blakesley@gmail.com>
2024-09-06 11:55:22 +0100

    Updated USB PID and descriptor strings

--- targetlibs/stm32usb/usbd_desc.c
+++ targetlibs/stm32usb/usbd_desc.c
@@ -56,16 +56,16 @@
 /** @defgroup USBD_DESC_Private_Defines
   * @{
   */ 
-#define USBD_VID     1155
+#define USBD_VID     0x0483 // STMicroelectronics VID (1155 decimal)
 #define USBD_LANGID_STRING     1033
-#define USBD_MANUFACTURER_STRING     "STMicroelectronics"
+#define USBD_MANUFACTURER_STRING     "The Wand Company"
 #ifndef USB_PRODUCT_ID
-  #define USB_PRODUCT_ID     22336
+  #define USB_PRODUCT_ID   0xA4DF // 0xA4DF is the PID assigned by ST for the Tricorder.  Currently awaiting a custom PID for the Pip-Boy. (ST's standard PID is 0x5740 / 22336 decimal)
 #endif
 #ifdef PIPBOY
-  #define USBD_PRODUCT_STRING_FS     "The Wand Company Pip-Boy"
+  #define USBD_PRODUCT_STRING_FS     "Vault-Tec Pip-Boy"
 #else
-  #define USBD_PRODUCT_STRING_FS     "STM32 Virtual ComPort"
+  #define USBD_PRODUCT_STRING_FS     "Virtual ComPort"
 #endif
 #define USBD_SERIALNUMBER_STRING_FS     "00000000001A"
 #define USBD_CONFIGURATION_STRING_FS     "CDC Config"
