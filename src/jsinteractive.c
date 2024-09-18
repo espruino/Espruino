espruino/Espruino

commit cc96161ec774f9b6389e81b255a5bc77cf569ab4

Fix regression with inserting chars mid-line after packet parsing added

src/jsinteractive.c
@@ -1146,6 +1146,8 @@ void jsiIsAboutToEditInputLine() {
       }
     }
   }
+  // Update the length
+  inputLineLength = (int)jsvGetStringLength(inputLine);
 }
 
 void jsiHandleDelete(bool isBackspace) {
@@ -1576,6 +1578,7 @@ void jsiPacketFileEnd() {
 
 void jsiPacketExit() {
   inputState = IPS_NONE;
+  inputPacketLength = 0;
   // cancel timeout
   // cancel timeout
   JsVar *timeout = jsvObjectGetChildIfExists(execInfo.hiddenRoot, "PK_TIMEOUT");

commit 9557264738dd68899606c17fc42846d8f468bd5f

Fix upload speed regression after cc96161ec774f9b6389e81b255a5bc77cf569ab4

src/jsinteractive.c
@@ -1147,7 +1147,8 @@ void jsiIsAboutToEditInputLine() {
     }
   }
   // Update the length
-  inputLineLength = (int)jsvGetStringLength(inputLine);
+  if (inputLineLength<0)
+    inputLineLength = (int)jsvGetStringLength(inputLine);
 }
 
 void jsiHandleDelete(bool isBackspace) {