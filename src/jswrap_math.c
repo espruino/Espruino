c8934a3dd5fccf07650449309fbe44cecae97d3f
Gordon Williams <gw@pur3.co.uk>
2024-09-23 09:45:33 +0100

    Ensure that 1%0.0===NaN (fix #2556)

--- src/jswrap_math.c
+++ src/jswrap_math.c
@@ -265,7 +265,7 @@ double jswrap_math_mod(double x, double y) {
   double a, b;
   const double c = x;
 
-  if (!isfinite(x) || isnan(y))
+  if (!isfinite(x) || isnan(y) || y==0)
     return NAN;
 
   if (0 > c) {
