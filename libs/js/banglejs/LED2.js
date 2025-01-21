e5d6433d1ea089138c434b8d5548d6de23416a93
Gordon Williams <gw@pur3.co.uk>
2025-01-21 09:46:11 +0000

    Bangle.js: Ensure fake LED1/LED2 remember state

--- libs/js/banglejs/LED2.js
+++ libs/js/banglejs/LED2.js
@@ -1,10 +1,9 @@
 (function() { // by @allObjects
   return { 
-    _v: false, // status on / off
-    read: function() { return this._v; },
+    read: function() { return !!global["\xff"].LED2; },
     set: function() { this.write(1); },
     reset: function() { this.write(0); },
-    write: function(v) { g.setColor((this._v=!!v)?"#0f0":g.theme.bg).fillCircle((g.getWidth()+20)/2,5,5);Bangle.setLCDPower(1); },
-    toggle: function() { this.write(!this._v); }
+    write: function(v) { g.setColor((global["\xff"].LED2=!!v)?"#0f0":g.theme.bg).fillCircle((g.getWidth()+20)/2,5,5);Bangle.setLCDPower(1); },
+    toggle: function() { this.write(!this.read()); }
   };
 })
