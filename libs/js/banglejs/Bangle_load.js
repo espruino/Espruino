8f0fe9e1246e3354b592928f92218036929d9f34
Gordon Williams <gw@pur3.co.uk>
2026-01-19 15:57:31 +0000

    Add `Bangle.showLoadingScreen()` and call it from `Bangle.load()`

--- libs/js/banglejs/Bangle_load.js
+++ libs/js/banglejs/Bangle_load.js
@@ -3,6 +3,7 @@
     Bangle.setUI(); // remove all existing UI (and call Bangle.uiRemove)
     __FILE__=name;
     if (!name) name = ".bootcde";
+    Bangle.showLoadingScreen(); // show loading screen
     setTimeout(eval,0,require("Storage").read(name)); // Load app without a reboot
   } else load((name!=".bootcde")?name:undefined);
 })
