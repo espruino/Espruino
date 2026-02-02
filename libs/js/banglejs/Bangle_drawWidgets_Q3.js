7b048640dc87d42c4841d05d56fbb49a55a190ce
Gordon Williams <gw@pur3.co.uk>
2026-02-02 09:27:46 +0000

    Bangle.js2: Ensure right-aligned widgets go right to the edge of the screen (#2686) (also remove tabs from code)

--- libs/js/banglejs/Bangle_drawWidgets_Q3.js
+++ libs/js/banglejs/Bangle_drawWidgets_Q3.js
@@ -1,18 +1,18 @@
 (function() {
   if (!global.WIDGETS) return;
   var w=g.getWidth(), h=g.getHeight(), pos={
-	  tl:{x:0, y:0, r:0, c:0}, // if r==1, we're right->left
-	  tr:{x:w, y:0, r:1, c:0},
-	  bl:{x:0, y:h-24, r:0, c:0},
-	  br:{x:w, y:h-24, r:1, c:0}
+    tl:{x:0, y:0, r:0, c:0}, // if r==1, we're right->left
+    tr:{x:w, y:0, r:1, c:0},
+    bl:{x:0, y:h-24, r:0, c:0},
+    br:{x:w, y:h-24, r:1, c:0}
   }, p;
   for (var wd of WIDGETS) {
     p = pos[wd.area];
-  	if (!p || wd.width == 0) continue;
-  	wd.x = p.x - p.r*wd.width;
-  	wd.y = p.y;
-  	p.x += wd.width*(1-2*p.r);
-  	p.c++;
+    if (!p || wd.width == 0) continue;
+    wd.x = p.x - p.r*wd.width;
+    wd.y = p.y;
+    p.x += wd.width*(1-2*p.r);
+    p.c++;
   }
   g.reset();
   if (pos.tl.c || pos.tr.c) g.clearRect(0,0,w-1,23);
