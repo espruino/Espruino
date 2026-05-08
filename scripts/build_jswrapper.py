3ed7b64611dc7152014cb650f5c223ae7add11db
Gordon Williams <gw@pur3.co.uk>
2026-05-08 08:37:18 +0100

    Fix tab complete on ESP8266 (which also accessed strOffset) by changing the macro - fix #2687

--- scripts/build_jswrapper.py
+++ scripts/build_jswrapper.py
@@ -429,11 +429,7 @@ JsVar *jswBinarySearch(const JswSymList *symbolsPtr, JsVar *parent, const char *
   while (searchMin <= searchMax) {
     int idx = (searchMin+searchMax) >> 1;
     const JswSymPtr *sym = &symbolsPtr->symbols[idx];
-#ifndef ESP8266 // normal method that supports ESPR_PACKED_SYMPTR
     int cmp = FLASH_STRCMP(name, &symbolsPtr->symbolChars[JSWSYMPTR_OFFSET(sym)]);
-#else // ESP8266 could be aligned so must be read with READ_FLASH_UINT16
-    int cmp = FLASH_STRCMP(name, &symbolsPtr->symbolChars[READ_FLASH_UINT16(&sym->strOffset)]);
-#endif
     if (cmp==0) {
       unsigned short functionSpec = READ_FLASH_UINT16(&sym->functionSpec);
       if ((functionSpec & JSWAT_EXECUTE_IMMEDIATELY_MASK) == JSWAT_EXECUTE_IMMEDIATELY)
