a2a748ee1c5b5ddbbf8e567aca0481cf3bd9d9de
Gordon Williams <gw@pur3.co.uk>
2026-09-21 09:40:17 +0100

    ESP32: Fix crash if WiFi disabled with Telnet included in build (Fix #2663) Manually merge in #2663 from @MaBecker

--- libs/network/telnet/jswrap_telnet.c
+++ libs/network/telnet/jswrap_telnet.c
@@ -20,6 +20,9 @@
 #include "socketserver.h"
 
 #include "../network.h"
+#if defined(ESP32)
+#include "jshardwareESP32.h"
+#endif
 
 #if defined(ESP8266)
 extern int os_printf_plus(const char *format, ...)  __attribute__((format(printf, 1, 2)));
@@ -121,7 +124,11 @@ void jswrap_telnet_init(void) {
 #ifdef LINUX
   tnSrvMode = telnetEnabled ? MODE_ON : MODE_OFF;
 #else
+#if defined (ESP32)
+  tnSrvMode = ESP32_Get_NVS_Status(ESP_NETWORK_WIFI) ? MODE_ON : MODE_OFF;
+#else
   tnSrvMode = MODE_ON; // hardcoded for now
+#endif
 #endif
 }
 
