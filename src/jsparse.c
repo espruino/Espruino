84c190da7feb10a976d7ca422be39adaa10fb3c2
Gordon Williams <gw@pur3.co.uk>
2026-09-25 09:54:31 +0100

    ESP32: Fix issue calling a bound function with no arguments

--- src/jsparse.c
+++ src/jsparse.c
@@ -538,6 +538,7 @@ NO_INLINE JsVar *jspeFunctionCall(JsVar *function, JsVar *functionName, JsVar *t
         if ((unsigned)argCount>=argPtrSize) {
           // allocate more space on stack if needed
           unsigned int newArgPtrSize = (argPtrSize?argPtrSize:(unsigned int)argCount)*4;
+          if (!newArgPtrSize) newArgPtrSize=4;
           size_t newArgPtrByteSize = sizeof(JsVar*)*newArgPtrSize;
           if (jsuGetFreeStack() < 256+newArgPtrByteSize) {
             jsExceptionHere(JSET_ERROR, "Insufficient stack for this many arguments");
